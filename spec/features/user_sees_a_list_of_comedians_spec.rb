716ad2de18e0627507861ffb1ce8b023fa1ab18c
mellisaluce <mjlgluce@gmail.com>
2018-10-02 19:03:04 -0600

    Adds Comedian Feature Tests

---
+++ spec/features/user_sees_a_list_of_comedians_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe 'Comedian Index Page' do
+  before(:each) do
+  end
+
+  it 'shows a list of all comedians' do
+    visit '/comedians'
+
+    expect(page).to have_content()
+    #expect comedian count to be
+  end
+
+  it 'shows a comedians name, age, and city' do
+    visit '/comedians'
+
+    expect(page).to have_content('Name: #')
+    expect(page).to have_content('Age: #')
+    expect(page).to have_content('City: #')
+
+  end
+end

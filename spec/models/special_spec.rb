8a5841e041f6f281ede85cbb9a1f7280b12b63d4
mellisaluce <mjlgluce@gmail.com>
2018-10-03 10:28:21 -0600

    Special Model Passes Validation Test

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -1,10 +1,11 @@
-# RSpec.describe Special do
-#   describe 'Validations' do
-#     describe 'Required Field(s)' do
-#       it 'should be invalid if missing a name' do
-#         special = Special.create
-#         expect(special).to_not be_valid
-#       end
-#     end
-#   end
-# end
+RSpec.describe Special do
+  describe 'Validations' do
+    describe 'Required Field(s)' do
+      it 'should be invalid if missing a name' do
+        special = Special.create(runtime: 60, image_url: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Amy%27s_smart_girls_logo.png", comedian_id: 1)
+
+        expect(special).to_not be_valid
+      end
+    end
+  end
+end

56d663f74a64a3bc1e2f9f6f8403477ebaef428e
mellisaluce <mjlgluce@gmail.com>
2018-10-02 20:41:32 -0600

    Added feature test

--- spec/features/user_sees_a_list_of_comedians_spec.rb
+++ spec/features/user_sees_a_list_of_comedians_spec.rb
@@ -1,20 +1,21 @@
-# RSpec.describe 'Comedian Index Page' do
-#   before(:each) do
-#   end
-#
-#   it 'shows a list of all comedians' do
-#     visit '/comedians'
-#
-#     expect(page).to have_content()
-#     #expect comedian count to be
-#   end
-#
-#   it 'shows a comedians name, age, and city' do
-#     visit '/comedians'
-#
-#     expect(page).to have_content('Name: #')
-#     expect(page).to have_content('Age: #')
-#     expect(page).to have_content('City: #')
-#
-#   end
-# end
+RSpec.describe 'Comedian Index Page' do
+  before(:each) do
+    @comedian_1 = Comedian.create(name: 'Barb', age: 30, city: 'Denver')
+    @comedian_2 = Comedian.create(name: 'Bert', age: 50, city: 'New York')
+  end
+
+  it 'shows a list of all comedians' do
+    visit '/comedians'
+
+    expect(page).to have_content(@comedian_1.name)
+    expect(page).to have_content(@comedian_2.name)
+  end
+
+  it 'shows a comedians name, age, and city' do
+    visit '/comedians'
+
+    expect(page).to have_content(@comedian_1.name)
+    expect(page).to have_content(@comedian_1.age)
+    expect(page).to have_content(@comedian_1.city)
+  end
+end

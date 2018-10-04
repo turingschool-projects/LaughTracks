286e7d223f53cfbf604201ab54ac100e1740d9bd
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-10-03 20:43:23 -0600

    fix browser issues

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,4 +1,8 @@
+require_relative '../models/comedian'
+
 class LaughTracksApp < Sinatra::Base
+  # set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/comedians' do
     @comedians = Comedian.all

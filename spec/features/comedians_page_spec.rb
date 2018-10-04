ec037719e6f9f11a875822d3d370b81529f5d45d
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-04 17:51:52 -0600

    added specials information and testing

--- spec/features/comedians_page_spec.rb
+++ spec/features/comedians_page_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe "a visitor visits the comedians page" do
     expect(page).to have_content(comic.city)
   end
 
+  it 'comedian should have many specials' do
+    association = Comedian.reflect_on_association(:specials)
+    expect(association.macro).to eq :has_many
+  end
+
+  it 'should show specials information'do
+    comic = Comedian.create(name: 'Mitch Hedberg', age: 48, city: 'Helena, Montana')
+    special = comic.specials.create(name: "Louis C.K. 2017", runtime: 74,
+                                    thumbnail: "https://m.media-amazon.com/images/M/MV5BOGYwMmVlMWUtZDE2My00MzMyLTk5MTQtZDdmM2U5YWQwZjM1XkEyXkFqcGdeQXVyMjExNDAyOTU@._V1_UY268_CR4,0,182,268_AL_.jpg"
+                                    )
+    visit '/comedians'
+
+    expect(page).to have_content(special.name)
+    expect(page).to have_content(special.runtime)
+    expect(page).to have_css("img[src='#{special.thumbnail}']")
+  end
+
 end

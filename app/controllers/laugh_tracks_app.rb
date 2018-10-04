035b526a73bff07d7cb42ade15332458a00835aa
Bailey Diveley <bailey.diveley@gmail.com>
2018-10-03 18:45:03 -0600

    Create comedian database and seed

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,10 @@
+require_relative '../models/comedian.rb'
+
 class LaughTracksApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/comedians' do
+    @comedians = Comedian.all
+    erb :'comedians/index'
+  end
 end
--- config.ru
+++ config.ru
@@ -3,4 +3,6 @@ Bundler.require
 
 require File.expand_path('../config/environment', __FILE__)
 
+require './app/controllers/laugh_tracks_app'
+
 run LaughTracksApp

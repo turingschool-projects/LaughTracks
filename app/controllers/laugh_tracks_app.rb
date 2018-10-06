54a78e4a148c8c48a0f12491de542e05a6417160
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-10-06 11:52:03 -0600

    query /commedians using age

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -5,7 +5,13 @@ class LaughTracksApp < Sinatra::Base
   set :method_override, true
 
   get '/comedians' do
-    @comedians = Comedian.all
+    # @comedians = Comedian.all.find_by(params[:age])
+    # @comedians = Comedian.all
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+    else
+      @comedians = Comedian.all
+    end
     erb :"comedians/index"
     #@comedians = Comedian.where(age: params[:age])
     #if params

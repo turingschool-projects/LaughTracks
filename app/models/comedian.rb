b4e9dcd775f668b9b32a575515246599e58e9ebd
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-10-07 16:01:12 -0600

    completes average age of comedians and test

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,8 @@
 class Comedian < ActiveRecord::Base
   validates_presence_of :name, :age, :city
   has_many :specials
+
+  def self.average_age
+   average(:age)
+  end
 end
--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe Comedian do
       end
     end
   end
+  describe 'Class Methods' do
+    it '.average_age' do
+      mitch = Comedian.create(name: 'Mitch Hedberg', age: 48, city: "Los Angeles")
+      hannah = Comedian.create(name: 'Hannah Hart', age: 32, city: "Los Angeles")
+      mamrie = Comedian.create(name: 'Mamrie Hard', age: 38, city: 'Los Angeles')
+
+      average_age = Comedian.average_age.to_f.round(2)
+
+      expect(average_age).to eq(39.33)
+    end
+  end
+
 end

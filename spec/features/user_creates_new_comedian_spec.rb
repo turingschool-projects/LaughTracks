d1ce9ac65da668741c488d6842bda8d515a1beb8
Bailey Diveley <bailey.diveley@gmail.com>
2018-10-06 22:24:14 -0600

    Fix functinality with form

--- spec/features/user_creates_new_comedian_spec.rb
+++ spec/features/user_creates_new_comedian_spec.rb
@@ -3,15 +3,13 @@ RSpec.describe 'a visitor creates a new comedian' do
     it 'should allow me to create a new comedian' do
       visit 'comedians/new'
 
-      comedian = Comedian.create(name: "Funny Bones", age: 36, city: "Toronto")
-
-      fill_in('comedian[name]', with: "Jim Gaffigan")
+      fill_in('comedian[name]', with: "Jeff Gaffigan")
       fill_in('comedian[age]', with: 47)
       fill_in('comedian[city]', with: "Elgin")
       click_button "Submit"
 
       expect(current_path).to eq('/comedians')
-      expect(page).to have_content('Jim Gaffigan')
+      expect(page).to have_content('Jeff Gaffigan')
     end
   end
 end

7741180fd0a0b2d984d0d7ee221f3c91567404c0
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-10-07 14:21:18 -0600

    passes feature test showing special on comedian page, updates comedian index to list

--- spec/features/user_can_see_comedians_on_page_index_spec.rb
+++ spec/features/user_can_see_comedians_on_page_index_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe "visitor visit comedian page" do
     expect(page).to have_content(hannah.name)
     expect(page).to have_content(hannah.age)
   end
+
+  it "vistor sees comedians specials listed" do
+    mitch = Comedian.create(name: 'Mitch Hedberg', age: 48, city: "Los Angeles")
+    hannah = Comedian.create(name: 'Hannah Hart', age: 32, city: "Los Angeles")
+
+    mitch_special = mitch.specials.create(name: "Mitch's special", thumbnail: "", run_time: 0)
+    hannah_special = hannah.specials.create(name: "Hannah's special", thumbnail: "", run_time: 90)
+
+
+    visit '/comedians'
+
+    expect(page).to have_content(mitch_special.name)
+    expect(page).to have_content(hannah_special.name)
+  end
+
+
 end

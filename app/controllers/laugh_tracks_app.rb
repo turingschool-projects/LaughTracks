8cb5e667a2293a5b85af1fb8f51b2bcef615e2ff
Tom Nunez <tomjnunez@gmail.com>
2018-10-08 05:32:21 -0600

    phase one complete

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,22 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    if params.empty?
+      @comedians = Comedian.all
+      @specials = Special.all
+    else
+      @comedians = Comedian.where(age:params[:age])
+      @specials = Special.where(comedian_id: @comedians.ids)
+    end
+    erb :"comedians"
+  end
+
+  get '/comedians/new' do
+    erb :"new"
+  end
+
+  post '/comedians' do
+    Comedian.create(params[:comedian])
+    redirect "/comedians"
+  end
 end

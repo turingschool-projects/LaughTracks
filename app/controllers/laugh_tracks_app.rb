c477636b034c1905711fdd00a7a9b7743029cce8
Ben Lee <bendelonlee@gmail.com>
2018-11-26 19:30:22 -0700

    Adds very basic controller, rendering one view which it also adds. Adds (my own) render_my_page method to controller. Basically, I wasn't sure how you render pages within a layout file. I wrote this method to do this. If it becomes apparent how to use Sinatra's native behavior to do this, this would be best practice.

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,18 @@
 class LaughTracksApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    render_my_page(:index, "Index")
+  end
+
+
+  private
+
+  def render_my_page(page, title = nil)
+    @page = page
+    title ||= page
+    @title = title
+    erb :layout
+  end
+
 end

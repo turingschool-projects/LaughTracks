a114c59d0eda6b6672c61682923333bc8adae51a
Brad Niedt <bradleyniedt@gmail.com>
2018-11-28 16:08:01 -0700

    Added validations to comedian and special models to pass validation tests

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,6 @@
 class Comedian < ActiveRecord::Base
   has_many :specials
+
+  validates :age, presence: true
+  validates :name, presence: true
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,5 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
+
+  validates :title, presence: true
 end

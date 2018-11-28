391d493db43df56a9fb8b2536a1236ebeb02649e
Brad Niedt <bradleyniedt@gmail.com>
2018-11-28 08:56:49 -0700

    Seed file populated with first data set

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
+require "./app/models/comedian"
 
+Comedian.destroy_all
+
+Comedian.create(name: "John Mulaney", age: 36, hometown: "Chicago, IL")
+Comedian.create(name: "Mitch Hedberg", age: 36, hometown: "Saint Paul, MN")
+Comedian.create(name: "Jim Gaffigan", age: 50, hometown: "Elgin, IL")
+Comedian.create(name: "Eddie Izzard", age: 56, hometown: "Colony of Aden, UK")
+Comedian.create(name: "Greg Proops", age: 59, hometown: "Phoenix, AZ")
+Comedian.create(name: "Colin Mochrie", age: 60, hometown: "Kilmarnok, Scotland")
+Comedian.create(name: "John Hodgman", age: 47, hometown: "Brookline, MA")
+Comedian.create(name: "Anjelah Johnson", age: 36, hometown: "San Jose, CA")
+Comedian.create(name: "Mike Birbiglia", age: 40, hometown: "Shrewsbury, MA")
+Comedian.create(name: "Aziz Ansari", age: 35, hometown: "Columbia, SC")
+Comedian.create(name: "Alex Wong", age: 36, hometown: "San Francisco, CA")
+Comedian.create(name: "Brian Reagan", age: 60, hometown: "Miami, FL")

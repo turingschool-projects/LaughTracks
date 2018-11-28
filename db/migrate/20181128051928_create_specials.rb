dac64c21d622f54a8ebb5e68d2d334185144bc48
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-28 15:29:09 -0700

    Gets databases up and running with tables connected together and populating data.

--- db/migrate/20181128051928_create_specials.rb
+++ db/migrate/20181128051928_create_specials.rb
@@ -4,6 +4,7 @@ class CreateSpecials < ActiveRecord::Migration[5.2]
       t.text :title
       t.integer :year
       t.integer :runtime
+      t.integer :comedian_id
       t.text :image
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_28_051928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comedians", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.text "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.text "title"
+    t.integer "year"
+    t.integer "runtime"
+    t.integer "comedian_id"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

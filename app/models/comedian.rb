574a6a3747e9bb7d4a5bcc7d1d8ec2dc94c4327c
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-27 18:40:09 -0700

    Adds method to comedian file to create comedians database.

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,11 @@
 class Comedian < ActiveRecord::Base
+  def change
+    create_table :comedians do |t|
+      t.text :name
+      t.integer :age
+      t.text :city
+
+      t.timestamps
+    end
+  end
 end

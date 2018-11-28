ea57038e276fdf5533ab80e1b9e93ad3571c562a
Ben Lee <bendelonlee@gmail.com>
2018-11-27 20:22:43 -0700

    Adds comedian image files in public and adds rough seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
+require "./app/models/comedian"
 
+Comedian.destroy_all
+
+Comedian.create(firstname:"Bob", lastname:"Newhart", age:89, city:"Los Angeles", image_path:"")
+Comedian.create(firstname:"Adam", lastname:"Burke", age:51, city:"Chicago", image_path:"")
+Comedian.create(firstname:"Cameron", lastname:"Esposito", age:38, city:"Los Angeles", image_path:"")
+Comedian.create(firstname:"Dave", lastname:"Chappelle", age:45, city:"Washington D.C.", image_path:"")
+Comedian.create(firstname:"Ellen", lastname:"Degeneres", age:60, city:"Los Angeles", image_path:"")
+Comedian.create(firstname:"Hannah", lastname:"Gadsby", age:40, city:"Smithton", image_path:"")
+Comedian.create(firstname:"Hannibal", lastname:"Buress", age:35, city:"Chicago", image_path:"")
+Comedian.create(firstname:"James", lastname:"Adomian", age:38, city:"New York", image_path:"")
+Comedian.create(firstname:"Maria", lastname:"Bamford", age:48, city:"Los Angeles", image_path:"")
+Comedian.create(firstname:"Pete", lastname:"Holmes", age:39, city:"Los Angeles", image_path:"")
+Comedian.create(firstname:"Steve", lastname:"Martin", age:73, city:"Los Angeles", image_path:"")
+Comedian.create(firstname:"Wanda", lastname:"Sykes", age:54, city:"Los Angeles", image_path:"")

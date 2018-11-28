ac47bf8c4c09f15b8cde8128682a9dfe9f047d90
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-11-28 16:42:41 -0700

    adds initial seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
+require './app/models/comedian'
+require './app/models/special'
 
+Comedian.destroy_all
+Special.destroy_all
+
+Comedian.create(name: 'Dane Cook', age: 32, city: 'Los Angeles')
+Comedian.create(name: 'Dane Cook', age: 32, city: 'Los Angeles')
+Comedian.create(name: 'Dane Cook', age: 32, city: 'Los Angeles')
+Comedian.create(name: 'Dane Cook', age: 32, city: 'Los Angeles')
+
+Special.create(name: 'Fluffy and Proud', comedian_id: 2, run_time_minutes: 60, image_url: 'adsda')
+Special.create(name: 'Fluffy and Proud', comedian_id: 2, run_time_minutes: 60, image_url: 'adsda')
+Special.create(name: 'Fluffy and Proud', comedian_id: 2, run_time_minutes: 60, image_url: 'adsda')
+Special.create(name: 'Fluffy and Proud', comedian_id: 2, run_time_minutes: 60, image_url: 'adsda')

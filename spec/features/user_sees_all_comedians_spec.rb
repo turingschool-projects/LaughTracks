51865e948bffb57642224e71f05bcf6756bc5548
Brad Niedt <bradleyniedt@gmail.com>
2018-11-28 16:36:11 -0700

    Adds code to pass first user story test

--- spec/features/user_sees_all_comedians_spec.rb
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe 'As a user' do
 
       visit '/comedians'
 
-      expect(page).to have_content(comedian_1.name)
-      expect(page).to have_content(comedian_1.age)
-      expect(page).to have_content(comedian_1.hometown)
+      expect(page).to have_content("Name: #{comedian_1.name}")
+      expect(page).to have_content("Age: #{comedian_1.age}")
+      expect(page).to have_content("Hometown: #{comedian_1.hometown}")
+
+      expect(page).to have_content("Name: #{comedian_2.name}")
+      expect(page).to have_content("Age: #{comedian_2.age}")
+      expect(page).to have_content("Hometown: #{comedian_2.hometown}")
     end
   end
 end

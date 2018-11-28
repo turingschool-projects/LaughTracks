1d422b6442f7fb2130e3290c6e6e391c5ff28c8b
Brad Niedt <bradleyniedt@gmail.com>
2018-11-28 15:45:06 -0700

    Adds Special model and revised seed file to create specials with comedian relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_015822) do
+ActiveRecord::Schema.define(version: 2018_11_28_222518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,9 @@ ActiveRecord::Schema.define(version: 2018_11_28_015822) do
   create_table "specials", force: :cascade do |t|
     t.text "title"
     t.integer "runtime"
-    t.text "image_file"
-    t.text "comedian_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comedian_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,31 @@
 require "./app/models/comedian"
+require "./app/models/special"
 
 Comedian.destroy_all
+Special.destroy_all
 
-Comedian.create(name: "John Mulaney", age: 36, hometown: "Chicago, IL")
-Comedian.create(name: "Mitch Hedberg", age: 36, hometown: "Saint Paul, MN")
-Comedian.create(name: "Jim Gaffigan", age: 50, hometown: "Elgin, IL")
-Comedian.create(name: "Eddie Izzard", age: 56, hometown: "Colony of Aden, UK")
-Comedian.create(name: "Greg Proops", age: 59, hometown: "Phoenix, AZ")
-Comedian.create(name: "Colin Mochrie", age: 60, hometown: "Kilmarnok, Scotland")
-Comedian.create(name: "John Hodgman", age: 47, hometown: "Brookline, MA")
-Comedian.create(name: "Anjelah Johnson", age: 36, hometown: "San Jose, CA")
-Comedian.create(name: "Mike Birbiglia", age: 40, hometown: "Shrewsbury, MA")
-Comedian.create(name: "Aziz Ansari", age: 35, hometown: "Columbia, SC")
-Comedian.create(name: "Alex Wong", age: 36, hometown: "San Francisco, CA")
-Comedian.create(name: "Brian Reagan", age: 60, hometown: "Miami, FL")
+comedian_1 = Comedian.create(name: "John Mulaney", age: 36, hometown: "Chicago, IL")
+comedian_2 = Comedian.create(name: "Mitch Hedberg", age: 36, hometown: "Saint Paul, MN")
+comedian_3 = Comedian.create(name: "Jim Gaffigan", age: 50, hometown: "Elgin, IL")
+comedian_4 = Comedian.create(name: "Eddie Izzard", age: 56, hometown: "Colony of Aden, UK")
+comedian_5 = Comedian.create(name: "Greg Proops", age: 59, hometown: "Phoenix, AZ")
+comedian_6 = Comedian.create(name: "Colin Mochrie", age: 60, hometown: "Kilmarnok, Scotland")
+comedian_7 = Comedian.create(name: "John Hodgman", age: 47, hometown: "Brookline, MA")
+comedian_8 = Comedian.create(name: "Anjelah Johnson", age: 36, hometown: "San Jose, CA")
+comedian_9 = Comedian.create(name: "Mike Birbiglia", age: 40, hometown: "Shrewsbury, MA")
+comedian_10 = Comedian.create(name: "Aziz Ansari", age: 35, hometown: "Columbia, SC")
+comedian_11 = Comedian.create(name: "Alex Wong", age: 36, hometown: "San Francisco, CA")
+comedian_12 = Comedian.create(name: "Brian Reagan", age: 60, hometown: "Miami, FL")
+
+comedian_1.specials.create(title: "Kid Gorgeous", runtime: 65)
+comedian_2.specials.create(title: "Comedy Central Presents: Mitch Hedberg", runtime: 30)
+comedian_3.specials.create(title: "Cinco", runtime: 73)
+comedian_4.specials.create(title: "Eddie Izzard Live at Madison Square Garden", runtime: 86)
+comedian_5.specials.create(title: "Live at Musso and Frank Grill", runtime: 60)
+comedian_6.specials.create(title: "Not Made Up Special Name", runtime: 6)
+comedian_7.specials.create(title: "Ragnarok", runtime: 67)
+comedian_8.specials.create(title: "Not Fancy", runtime: 90)
+comedian_9.specials.create(title: "Thank God For Jokes", runtime: 70)
+comedian_10.specials.create(title: "Buried Alive", runtime: 80)
+comedian_11.specials.create(title: "Hardknock Wife", runtime: 64)
+comedian_12.specials.create(title: "Brian Reagan", runtime: 60)

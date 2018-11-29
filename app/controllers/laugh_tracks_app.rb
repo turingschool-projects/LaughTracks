95784887e6a837145eb486f9544882ac139213e2
Brad Niedt <bradleyniedt@gmail.com>
2018-11-29 15:02:47 -0700

    removes new route and adds class method to sort comedians

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,13 +1,7 @@
 class LaughTracksApp < Sinatra::Base
   get '/comedians' do
-    @comedians = Comedian.all
+    @comedians = Comedian.sorter(params)
     @specials = Special.all
     erb :index
   end
-
-  get '/comedians/:age' do
-    @comedians = Comedian.find_by_age(params[:age])
-    @specials = Comedian.find_by_age(params[:age]).specials
-    erb :index
-  end 
 end

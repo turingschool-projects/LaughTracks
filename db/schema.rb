7145569f4a05214fcbccdf1bb7850c9b770dc830
Mary Goodhart <marya.grace5@gmail.com>
2018-11-28 17:19:20 -0700

    adds comedian index schema seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_151116) do
+ActiveRecord::Schema.define(version: 2018_11_28_225826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_11_28_151116) do
     t.text "name"
     t.integer "age"
     t.text "hometown"
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.text "name"
+    t.integer "run_time"
+    t.integer "comedian_id"
   end
 
 end

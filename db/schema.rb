8818794f5082cb71ff6b2817057c88c1a63988fd
abenetka <ali.benetka@gmail.com>
2018-11-28 21:52:41 -0700

    Adds migration for comedian_id to specials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_231348) do
+ActiveRecord::Schema.define(version: 2018_11_29_042206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_231348) do
     t.integer "length"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comedian_id"
   end
 
 end

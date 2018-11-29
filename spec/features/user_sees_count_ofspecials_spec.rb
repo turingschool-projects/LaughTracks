e40e168b4027000da2000dbd5651bd37b2a259eb
Brad Niedt <bradleyniedt@gmail.com>
2018-11-29 15:33:16 -0700

    adds more specials to seed file and new feature test for seeing special count

---
+++ spec/features/user_sees_count_ofspecials_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe "As a user" do
+  describe "when I visit /comedians" do
+    it "should see a specials count for each comedian" do
+      comedian_1 = Comedian.create(name: "number 1", age: 36, hometown: "Chicago, IL")
+      comedian_2 = Comedian.create(name: "number 2", age: 36, hometown: "Saint Paul, MN")
+      comedian_1.specials.create(title: "special 1", runtime: 65, image_file: "https://m.media-amazon.com/images/M/MV5BOWZlYjE4ZDYtNjhlNi00ZGQ0LTgxZmUtNGRjNDM4YWJmOWNiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
+      comedian_1.specials.create(title: "special 2", runtime: 65, image_file: "https://m.media-amazon.com/images/M/MV5BOWZlYjE4ZDYtNjhlNi00ZGQ0LTgxZmUtNGRjNDM4YWJmOWNiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
+      comedian_1.specials.create(title: "special 3", runtime: 65, image_file: "https://m.media-amazon.com/images/M/MV5BOWZlYjE4ZDYtNjhlNi00ZGQ0LTgxZmUtNGRjNDM4YWJmOWNiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
+      comedian_2.specials.create(title: "special 12", runtime: 30, image_file: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGHGPBMGUNy3WnCVWPyiCQxtRZ_G4KC3Cjv1JiCmsq6rM6lk3q-g")
+
+      visit '/comedians'
+      
+      within("#comedian-1") do
+        expect(page).to have_content("Number of Specials: #{comedian_1.specials_count}")
+      end
+      within("#comedian-2") do
+        expect(page).to have_content("Number of Specials: #{comedian_1.specials_count}")
+      end
+    end
+  end
+end

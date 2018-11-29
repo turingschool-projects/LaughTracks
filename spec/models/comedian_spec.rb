75e4ce65634a432c0c360ed6d5d8f879d7be061d
Brad Niedt <bradleyniedt@gmail.com>
2018-11-29 11:33:44 -0700

    adds model test for comedian average age

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Comedian do
       end
     end
   end
+
+  describe "instance methods" do
+    it "should caluculate average age" do
+      comedian_1 = Comedian.create(name: "John Mulaney", age: 36, hometown: "Chicago, IL")
+      comedian_2 = Comedian.create(name: "Mitch Hedberg", age: 20, hometown: "Saint Paul, MN")
+      actual = Comedian.all.average_age
+
+      expect(actual).to eq(28)
+    end
+  end
 end

9fef1015f19eeb2d46405db94afc1840afd7c13e
abenetka <ali.benetka@gmail.com>
2018-11-29 15:22:37 -0700

    Adds test for average run length of tv specials

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe Special do
       end
     end
   end
+
+  describe "Class Methods" do
+    describe ".average_run_time" do
+      it "returns average run time of specials" do
+        comic_1 = Comedian.create(name: 'Mitch Hedberg', age: 37)
+        comic_2 = Comedian.create(name: 'Andrew Bueno', age: 33)
+
+        comic_1.specials.create(name: "Hell Yeah", length: 65)
+        comic_1.specials.create(name: "Funny thingT", length: 75)
+        comic_2.specials.create(name: "I am Funny", length: 50)
+        comic_2.specials.create(name: "You are not Funny", length: 60)
+
+        expect(Special.average_run_time).to eq(62.5)
+      end
+    end
+  end
 end

02567aa2683fe6d9e339b21b61896502bdad7f9e
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-28 18:25:38 -0700

    Adds logic to index to only see a certain comedians special, not all of them.

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,6 @@
 class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     @comedians = Comedian.all
-    @specials = Special.all
     erb :index
   end
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,7 +1,9 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
 
-  def find_a_comedians_specials
-
+  def find_a_comedians_specials(comedian_id)
+    if comedian_id == special.comedian_id
+      
+    end
   end
 end

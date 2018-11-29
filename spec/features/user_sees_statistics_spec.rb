4c3105799da2801c6b5cd7d67e3d7aa01954ddff
Brad Niedt <bradleyniedt@gmail.com>
2018-11-29 13:08:43 -0700

    adds feature test for average runtime stat

--- spec/features/user_sees_statistics_spec.rb
+++ spec/features/user_sees_statistics_spec.rb
@@ -23,5 +23,18 @@ RSpec.describe "As a user" do
         expect(page).to have_content(expected)
       end
     end
+    it "should see the average run time of all specials" do
+      comedian_1 = Comedian.create(name: "John Mulaney", age: 36, hometown: "Chicago, IL")
+      comedian_2 = Comedian.create(name: "Mitch Hedberg", age: 20, hometown: "Saint Paul, MN")
+      comedian_1.specials.create(title: "Kid Gorgeous", runtime: 65, image_file: "https://m.media-amazon.com/images/M/MV5BOWZlYjE4ZDYtNjhlNi00ZGQ0LTgxZmUtNGRjNDM4YWJmOWNiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
+      comedian_2.specials.create(title: "Comedy Central Presents: Mitch Hedberg", runtime: 30, image_file: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGHGPBMGUNy3WnCVWPyiCQxtRZ_G4KC3Cjv1JiCmsq6rM6lk3q-g")
+
+      visit '/comedians'
+
+      expected = Comedians.average_runtime
+      within("#statistics") do
+        expect(page).to have_content(expected)
+      end
+    end
   end
 end

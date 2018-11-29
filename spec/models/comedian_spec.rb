044a0e827d33e2654d31f0d6ab7bc4e281465410
abenetka <ali.benetka@gmail.com>
2018-11-29 15:20:53 -0700

    Adds test for average age of comedian

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Comedian do
       end
     end
   end
+
+  describe "Class Methods" do
+    describe ".average_age" do
+      it "returns average age of comedians" do
+        comic_1 = Comedian.create(name: 'Mitch Hedberg', age: 37)
+        comic_2 = Comedian.create(name: 'Andrew Bueno', age: 33)
+        comic_3 = Comedian.create(name: 'Martin Short', age: 68)
+
+        expect(Comedian.average_age).to eq(46)
+      end
+    end
+  end
 end

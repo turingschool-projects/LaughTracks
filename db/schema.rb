9f5d339187608ecfad0444e15bdfad37c4686628
Brad Niedt <bradleyniedt@gmail.com>
2018-11-28 18:47:20 -0700

    Ran a new migration to add in image urls

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_222518) do
   create_table "specials", force: :cascade do |t|
     t.text "title"
     t.integer "runtime"
+    t.text "image_file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "comedian_id"

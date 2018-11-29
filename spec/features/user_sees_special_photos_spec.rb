df290f7a53b5aff9d903c229a3e4c80fef27ff47
Brad Niedt <bradleyniedt@gmail.com>
2018-11-29 10:15:33 -0700

    Created new feature test for special thumbnail photos

--- spec/features/user_sees_special_photos_spec.rb
+++ spec/features/user_sees_special_photos_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe 'As a User' do
-  describe 'When I visit /comedians' do
-    it 'should see specials for comedians' do
+RSpec.describe "As a user" do
+  describe "when I visit /comedians" do
+    it "I should see a thumbnail image for the special" do
       comedian_1 = Comedian.create(name: "John Mulaney", age: 36, hometown: "Chicago, IL")
       comedian_2 = Comedian.create(name: "Mitch Hedberg", age: 36, hometown: "Saint Paul, MN")
       comedian_1.specials.create(title: "Kid Gorgeous", runtime: 65, image_file: "https://m.media-amazon.com/images/M/MV5BOWZlYjE4ZDYtNjhlNi00ZGQ0LTgxZmUtNGRjNDM4YWJmOWNiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
@@ -8,15 +8,8 @@ RSpec.describe 'As a User' do
 
       visit '/comedians'
 
-      within("#special-" + "#{comedian_1.specials.first.id}") do
-        expect(page).to have_content(comedian_1.specials.first.title)
-        expect(page).to have_content(comedian_1.specials.first.runtime)
-      end
-
-      within("#special-" + "#{comedian_2.specials.first.id}") do
-        expect(page).to have_content(comedian_2.specials.first.title)
-        expect(page).to have_content(comedian_2.specials.first.runtime)
-      end
+      expect(page).to have_css("img[src*='https://m.media-amazon.com/images/M/MV5BOWZlYjE4ZDYtNjhlNi00ZGQ0LTgxZmUtNGRjNDM4YWJmOWNiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg']")
+      expect(page).to have_css("img[src*='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGHGPBMGUNy3WnCVWPyiCQxtRZ_G4KC3Cjv1JiCmsq6rM6lk3q-g']")
     end
   end
 end

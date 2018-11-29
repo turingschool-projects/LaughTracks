c4a83d2837d08db32fb7864d9cfcae6190f49a3f
William Petty <william@snakeweed.com>
2018-11-29 13:35:16 -0700

    Validation testing established and working

--- app/models/symphony.rb
+++ app/models/symphony.rb
@@ -1,4 +1,9 @@
 class Symphony < ActiveRecord::Base
   belongs_to :composer
+
+  validates :name, presence: true
+  validates :key, presence: true
+  validates :year, presence: true
+  validates :composer_id, presence: true
   
 end
--- spec/models/composer_spec.rb
+++ spec/models/composer_spec.rb
@@ -2,15 +2,31 @@ RSpec.describe Composer do
   describe 'Validations' do
     describe 'Required Field(s)' do
 
-      it 'is invalid without a surname' do
-        comp = Composer.create(forename: "Ludwig van", city: "Vienna", dob: 1770, dod: 1827, age: 56)
+      Composer.create(surname: "Beethoven", forename: "Ludwig van", city: "Vienna", dob: 1770, dod: 1827, age: 56, thumbnail: "/images/beethoven.jpg")
 
+
+      it 'is invalid without a surname' do
+        comp = Composer.create(forename: "Franz Joseph", city: "Vienna", dob: 1732, dod: 1809, age: 77, thumbnail: "/images/haydn.jpg")
+        expect(comp).to_not be_valid
+      end
+
+      it 'is invalid witout a forename' do
+        comp = Composer.create(surname: "Beethoven", city: "Vienna", dob: 1770, dod: 1827, age: 56, thumbnail: "/images/beethoven.jpg")
+        expect(comp).to_not be_valid
+      end
+
+      it 'is invalid witout a city' do
+        comp = Composer.create(surname: "Beethoven", forename: "Ludwig van", dob: 1770, dod: 1827, age: 56, thumbnail: "/images/beethoven.jpg")
         expect(comp).to_not be_valid
       end
 
       it 'is invalid witout an age' do
         comp = Composer.create(surname: "Beethoven", forename: "Ludwig van", city: "Vienna", dob: 1770, dod: 1827)
+        expect(comp).to_not be_valid
+      end
 
+      it 'is invalid without a thumbnail' do
+        comp = Composer.create(surname: "Beethoven", forename: "Ludwig van", city: "Vienna", dob: 1770, dod: 1827, age: 56)
         expect(comp).to_not be_valid
       end
 

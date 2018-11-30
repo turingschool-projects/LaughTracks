510343b3c8a39b6d52d806d0f6ab25875f80bcec
Anten Skrabec <cebarks@gmail.com>
2018-11-30 10:45:29 -0700

    user story 1 impl and basic site structure

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,10 @@
 class LaughTracksApp < Sinatra::Base
+  get '/' do
+    redirect '/comedians'
+  end
+
+  get '/comedians' do
+    @comedians = Comedian.all
+    erb :index, layout: :layout
+  end
 end

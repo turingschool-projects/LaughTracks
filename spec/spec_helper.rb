ef403e6c5cbd223f1b8788375d8a03da17a371cd
abenetka <ali.benetka@gmail.com>
2018-11-30 12:01:21 -0700

    Reformats index page and spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,11 @@ ENV["RACK_ENV"] ||= "test"
 
 require 'bundler'
 Bundler.require(:default, :test)
-
 require 'simplecov'
 SimpleCov.start
 require File.expand_path('../../config/environment.rb', __FILE__)
 
 require 'capybara/dsl'
-
 Capybara.app = LaughTracksApp
 Capybara.save_path = 'tmp/capybara'
 

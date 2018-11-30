a1a8c0f2fdea51f0a214fe0183b4a3ef2070500b
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-11-30 16:36:02 -0700

    adds pluck to uniq_hometowns

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -7,11 +7,15 @@ class Comedian < ActiveRecord::Base
     average(:age)
   end
 
-  def self.uniq_hometowns
-    select(:city).distinct
+  def self.uniq_hometowns(caller = self)
+    select(:city).distinct.pluck(:city)
   end
 
   def self.find_by_age(age, comparison = false)
     where("age = '#{age}'")
+  end
+
+  def self.average_from_query(age)
+    find_by_age(age).average(:age)
   end
 end

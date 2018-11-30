6d0213817772a6e7e6525d3d32ff6d54717edf40
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-11-30 11:07:56 -0700

    app/controllers/laugh_tracks_app.rb

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -5,11 +5,14 @@ class LaughTracksApp < Sinatra::Base
   end
 
   get '/comedians' do
-    @comedians = Comedian.all
-    @average_age = Comedian.average_age
-    @average_length = Special.average_length
-    @uniq_hometowns = Comedian.uniq_hometowns
-
+    if params.empty?
+      @comedians = Comedian.all
+      @average_age = Comedian.average_age
+      @average_length = Special.average_length
+      @uniq_hometowns = Comedian.uniq_hometowns
+    else
+      @comedians = Comedian.find_by_age(params[:age])
+    end
     erb :index
   end
 

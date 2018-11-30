d5535676b582e005cab9165c5bfc90d61263e8d9
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-11-30 09:58:29 -0700

    adds average_length for special run time

--- app/models/special.rb
+++ app/models/special.rb
@@ -2,4 +2,8 @@ class Special < ActiveRecord::Base
   belongs_to :comedian
 
   validates_presence_of :name, :comedian_id, :run_time_minutes, :image_url
+
+  def self.average_length
+    average(:run_time_minutes)
+  end
 end

e67a50ef7aca4b822bd0ed4ba10f60451d860399
Ben Lee <bendelonlee@gmail.com>
2018-11-30 11:41:23 -0700

    Adds model tests to cover models with a coverage of 100%

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -34,11 +34,13 @@ RSpec.describe Comedian do
   end
   describe 'Class Methods' do
     before(:each) do
-      Comedian.create(age: 48, firstname: "Bozo", city: 'Dublin')
-      Comedian.create(age: 49, firstname: "Bozohead", city: 'Dublin')
-      Comedian.create(age: 50, firstname: "Bozonose", city: 'Topeka')
-      Comedian.create(age: 51, firstname: "Bozotoes", city: 'Topeka')
-      Comedian.create(age: 52, firstname: "Bozohands", city: 'Topeka')
+      @comic_1 = Comedian.create(age: 48, firstname: "Bozo", city: 'Dublin')
+      @comic_2 = Comedian.create(age: 49, firstname: "Bozohead", city: 'Dublin')
+      @comic_3 = Comedian.create(age: 50, firstname: "Bozonose", city: 'Topeka')
+      @comic_4 = Comedian.create(age: 51, firstname: "Bozotoes", city: 'Topeka')
+      @comic_5 = Comedian.create(age: 52, firstname: "Bozofeet", city: 'Topeka')
+      @comic_6 = Comedian.create(age: 52, firstname: "Bozohands", city: 'Topeka')
+      @comic_7 = Comedian.create(age: 48, firstname: "Bozoface", city: 'Dublin')
     end
       it 'returns average_age correctly' do
         expect(Comedian.average_age).to eq(50)
@@ -52,11 +54,36 @@ RSpec.describe Comedian do
         expected = {
           comedians: Comedian.all,
           cities: %w(Topeka Dublin),
-          average_age: 50,
+          average_age: 50
         }
         expect(Comedian.fetch_data(params)).to eq(expected)
-
       end
-
+      it 'fetches all data correctly with given age' do
+        params = {age: 48}
+        expected = {
+          comedians: [@comic_1, @comic_7],
+          cities: %w(Dublin),
+          average_age: 48
+        }
+        expect(Comedian.fetch_data(params)).to eq(expected)
+      end
+      it 'fetches comedians correctly when sorting by age' do
+        params = {sort: "age"}
+        expected = [@comic_7, @comic_1, @comic_2, @comic_3, @comic_4, @comic_6, @comic_5]
+        result_comedians =  Comedian.fetch_data(params)[:comedians]
+        expect(result_comedians).to eq(expected)
+      end
+      it 'fetches comedians correctly when sorting by name' do
+        params = {sort: "name"}
+        expected = [@comic_1, @comic_7, @comic_5, @comic_6, @comic_2, @comic_3, @comic_4]
+        result_comedians =  Comedian.fetch_data(params)[:comedians]
+        expect(result_comedians).to eq(expected)
+      end
+      it 'fetches comedians correctly when sorting by city' do
+        params = {sort: "city"}
+        expected = [@comic_2, @comic_1, @comic_7, @comic_6, @comic_4, @comic_3, @comic_5]
+        result_comedians =  Comedian.fetch_data(params)[:comedians]
+        expect(result_comedians).to eq(expected)
+      end
     end
   end
--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -14,7 +14,46 @@ RSpec.describe Special do
       special = Special.create!(title: "Monkeytown", runtime: 40)
       special = Special.create!(title: "Monkeytown 2",runtime: 50)
       expect(Special.average_runtime).to eq(45)
+
+    end
+    describe 'it fetches data correctly' do
+      before(:each) do
+        @comic_1 = Comedian.create!(age: 48, firstname: "Bozo", city: 'Dublin')
+        @comic_2 = Comedian.create!(age: 48, firstname: "Bozohead", city: 'Dublin')
+        @comic_3 = Comedian.create!(age: 50, firstname: "Bozonose", city: 'Topeka')
+
+        @special_1 = Special.create!(title: "Monkeytown", runtime: 40)
+        @comic_1.specials << @special_1
+
+        @special_2 = Special.create!(title: "I'm a clown", runtime: 50)
+        @special_3 = Special.create!(title: "I'm a clown 2", runtime: 60)
+        @comic_2.specials << @special_2
+        @comic_2.specials << @special_3
+
+        @special_4 = Special.create!(title: "Bignose!", runtime: 70)
+        @special_5 = Special.create!(title: "Bignose 2!", runtime: 80)
+        @special_6 = Special.create!(title: "Bignose 3!", runtime: 90)
+        @comic_3.specials << @special_4
+        @comic_3.specials << @special_5
+        @comic_3.specials << @special_6
+      end
+
+      it 'fetches all data correctly with empty params' do
+        params = {}
+        expected = {
+          average_runtime: 65,
+          total_specials: 6
+        }
+        expect(Special.fetch_data(params)).to eq(expected)
+      end
+      it 'fetches all data correctly with empty params' do
+        params = {age: 48}
+        expected = {
+          average_runtime: 50,
+          total_specials: 3
+        }
+        expect(Special.fetch_data(params)).to eq(expected)
+      end
     end
   end
-
 end

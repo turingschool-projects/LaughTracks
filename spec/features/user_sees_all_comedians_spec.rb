f5222f2df19c243b12418e693211e5fd142ca773
Brad Niedt <bradleyniedt@gmail.com>
2018-12-01 10:49:32 -0700

    adds styling for grid layout with flexbox within.

--- spec/features/user_sees_all_comedians_spec.rb
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'As a user' do
       visit '/comedians'
 
       within("#comedian-" + "#{comedian_1.id}") do
-        expect(page).to have_content("Name: #{comedian_1.name}")
+        expect(page).to have_content("#{comedian_1.name}")
         expect(page).to have_content("Age: #{comedian_1.age}")
         expect(page).to have_content("Hometown: #{comedian_1.hometown}")
       end
 
       within("#comedian-#{comedian_2.id}") do
-        expect(page).to have_content("Name: #{comedian_2.name}")
+        expect(page).to have_content("#{comedian_2.name}")
         expect(page).to have_content("Age: #{comedian_2.age}")
         expect(page).to have_content("Hometown: #{comedian_2.hometown}")
       end

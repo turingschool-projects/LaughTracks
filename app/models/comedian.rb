4e14d6de5e94e2b5fb364679805c8e42799d9f6b
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-11-30 18:19:41 -0700

    adds retrieving average age and hometowns from association other than self

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -3,19 +3,15 @@ class Comedian < ActiveRecord::Base
 
   validates_presence_of :name, :age, :city
 
-  def self.average_age
-    average(:age)
+  def self.average_age(caller = self)
+    caller.average(:age)
   end
 
   def self.uniq_hometowns(caller = self)
-    select(:city).distinct.pluck(:city)
+    caller.select(:city).distinct.pluck(:city)
   end
 
   def self.find_by_age(age, comparison = false)
     where("age = '#{age}'")
-  end
-
-  def self.average_from_query(age)
-    find_by_age(age).average(:age)
   end
 end

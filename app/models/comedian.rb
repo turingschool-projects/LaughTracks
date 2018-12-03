47baca8df962cf386b9682517c19693b89b0f1b1
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-02 17:36:16 -0700

    adds searching by <,>,<=,>= to find_by_age

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -11,7 +11,16 @@ class Comedian < ActiveRecord::Base
     caller.select(:city).pluck(:city).uniq
   end
 
-  def self.find_by_age(age, caller = self)
-    caller.where("age = '#{age}'")
+  def self.find_by_age(param, caller = self)
+    query = ""
+    query = "age = '#{param}'" if param =~ /^\d+$/
+    if param =~ /(<|>)=\d+/
+      operator,e,*age = param.split("")
+      query = "age #{operator}= '#{age.join}'"
+    elsif param =~ /(<|>)\d+/
+      operator,*age = param.split("")
+      query = "age #{operator} '#{age.join}'"
+    end
+    caller.where(query)
   end
 end

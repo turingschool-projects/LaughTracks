e714537c4633662946d485d6a11df9a578613068
April Dagonese <adagonese@gmail.com>
2019-01-21 12:55:48 -0700

    Adds New view with form

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,6 +2,10 @@ class LaughTracksApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/' do
-    'hello, world!'
+    erb :comedians
+  end
+
+  get '/new' do
+    erb :new
   end
 end

0d1c650c1782bee00d21aac47884057079033768
Ty Mazey <tymazey@gmail.com>
2019-01-24 15:37:35 -0700

    migration db for comedians and seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_24_220849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comedians", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.text "born"
+    t.boolean "deceased", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
+require_relative '../app/models/comedian.rb'
 
+Comedian.destroy_all
+Comedian.create(name: "Norm Macdonald", age: 59, born: "Quebec City, Quebec")
+Comedian.create(name: "Dave Chappelle", age: 45, born: "Washington, District of Columbia")
+Comedian.create(name: "Chris Rock", age: 53, born: "Andrews, South Carolina")
+Comedian.create(name: "Kevin Hart", age: 39, born: "Philidelphia, Pensylvenia")
+Comedian.create(name: "Jim gaffigan", age: 52, born: "elgin, Illinois")
+Comedian.create(name: "Bill Burr", age: 50, born: "Canton, Massachusetts")
+Comedian.create(name: "Eddie Murphy", age: 57, born: "Brooklyn, New York")
+Comedian.create(name: "Ellen DeGeneres", age: 60, born: "Metairie, Louisiana")
+Comedian.create(name: "Louis C.K.", age: 51, born: "Washington, District of Columbia")
+Comedian.create(name: "George Carlin", age: 71, born: "Manhattan, New York", deceased: true)
+Comedian.create(name: "Mitch Hedberg", age: 37, born: "St. Paul, Minnisota", deceased: true)
+Comedian.create(name: "Daniel Tosh", age: 43, born: "Boppad, Germany")

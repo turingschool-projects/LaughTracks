0630a58495a42acbd68668cce43406b18fdc4be5
Chris Lewis <13chlewis@gmail.com>
2019-01-23 19:03:51 -0700

    Updated migration file with special title

--- db/migrate/20190124015329_create_specials.rb
+++ db/migrate/20190124015329_create_specials.rb
@@ -1,8 +1,9 @@
 class CreateSpecials < ActiveRecord::Migration[5.2]
   def change
     create_table :specials do |t|
+      t.text    :title
       t.integer :run_time
-      t.text :image_url
+      t.text    :image_url
       t.integer :comedian_id
     end
   end

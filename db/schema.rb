31a8063c70c7b4924811e2acc1951cf08be2eace
Chris Lewis <13chlewis@gmail.com>
2019-01-23 18:05:24 -0700

    Ran comedians migration and created table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_24_005740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comedians", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.text "city"
+  end
 
 end

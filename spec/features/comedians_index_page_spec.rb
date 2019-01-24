1bf1dbfe44909d7f15010f84fa409f5be4cdec4d
Chris Lewis <13chlewis@gmail.com>
2019-01-24 15:21:44 -0700

    Feature test written for user story 4

--- spec/features/comedians_index_page_spec.rb
+++ spec/features/comedians_index_page_spec.rb
@@ -49,6 +49,14 @@ RSpec.describe "comedians index page" do
       Comedian.create(name: 'Bill Burr', age: 50, city: 'Canton')
       Special.create(title: 'John Mulaney: New in Town', run_time: 60, image_url: 'https://m.media-amazon.com/images/M/MV5BMjIzNzg5NTA2M15BMl5BanBnXkFtZTgwOTY4NjA2MDE@._V1_UY268_CR9,0,182,268_AL_.jpg', comedian_id: 1)
       Special.create(title: "Bill Burr: I'm Sorry You Feel That Way", run_time: 80, image_url: 'https://m.media-amazon.com/images/M/MV5BMTk0MzI5MjAxNV5BMl5BanBnXkFtZTgwNDY1NzA0NDE@._V1_UX182_CR0,0,182,268_AL_.jpg', comedian_id: 2)
+
+      visit '/comedians'
+
+      within '#statistics' do
+        expect(page).to have_content(43)
+        expect(page).to have_content(70)
+        expect(page).to have_content("Chicago, Canton")
+      end
     end
 
     it "shows a count of TV specials for each comedian and the Statistics show the total count of TV specials for every comedian" do

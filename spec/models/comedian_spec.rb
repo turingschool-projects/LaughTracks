5e6e975a6ac049b3b5d2c3f9e8f4dda22ecaac54
Ty Mazey <tymazey@gmail.com>
2019-01-25 11:22:07 -0700

    adds model test for average comedian age

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -14,7 +14,19 @@ RSpec.describe Comedian do
       it 'should be invalid if missing city born' do
         comic = Comedian.create(name: 'Mitch Hedberg', age: 37)
         expect(comic).to_not be_valid
-      end 
+      end
+    end
+  end
+
+  describe 'Class Methods' do
+    describe '.average_age' do
+      it 'returns the average age of all comedians'do
+        Comedian.create(name: 'Mitch Hedberg', born: 'St. Paul', age: 10)
+        Comedian.create(name: 'Mitch Hedberg', born: 'St. Paul', age: 20)
+        Comedian.create(name: 'Mitch Hedberg', born: 'St. Paul', age: 30)
+
+        expect(Comedian.average_age).to eq(20)
+      end
     end
   end
 end

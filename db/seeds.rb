ad92bf30639b0da9d06beb0aee63af7b515d5a64
earl-stephens <stephensearl@hotmail.com>
2019-01-25 09:55:24 -0700

    updated cities in comedian spreadsheet and added commedians to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
+require_relative '../app/models/comedian.rb'
+require_relative '../app/models/special.rb'
 
+Comedian.create(name: "Ellen DeGeneres", age: 61, city: "Metairie")
+Comedian.create(name: "Anjelah Johnson-Reyes", age: 36, city: "San Jose")
+Comedian.create(name: "Wanda Sykes", age: 54, city: "Portsmouth")
+Comedian.create(name: "Bob Newhart", age: 89, city: "Oak Park")
+Comedian.create(name: "Carol Burnett", age: 85, city: "San Antonio")
+Comedian.create(name: "Daniel Tosh", age: 43, city: "West Germany")
+Comedian.create(name: "Chris Rock", age: 53, city: "Andrews")
+Comedian.create(name: "Ron White", age: 62, city: "Fritch")
+Comedian.create(name: "Larry The Cable Guy", age: 55, city: "Pawnee City")
+Comedian.create(name: "Eddie Murphy", age: 57, city: "Brooklyn")
+Comedian.create(name: "Tim Allen", age: 65, city: "Denver")
+Comedian.create(name: "Dana Carvey", age: 63, city: "Missoula")

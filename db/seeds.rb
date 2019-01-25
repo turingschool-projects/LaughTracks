61389f3beb86d6b5ad0d2a1eb34b9ea9f5ff61ae
Julia Marco <marcootj@gmail.com>
2019-01-25 08:44:50 -0700

    Added comedians

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
+require_relative '../app/models/comedian.rb'
 
+Comedian.destroy_all
+
+
+Comedian.create(name: "John Mulaney", age: 36, city: "Chicago, IL")
+Comedian.create(name: "Chris Rock", age: 53, city: "Andrews, SC")
+Comedian.create(name: "Ellen Degeneres", age: 60, city: "Metairie, LA")
+Comedian.create(name: "Dave Chappelle", age: 45, city: "Washington D.C")
+Comedian.create(name: "Michelle Wolf", age: 33, city: "Hershey, PA")
+Comedian.create(name: "Jim Jefferies", age: 41, city: "Sydney")
+Comedian.create(name: "Tom Segura", age: 39, city: "Cincinnati, OH")
+Comedian.create(name: "Chris D'elia", age: 38, city: "Montclair, NJ")
+Comedian.create(name: "Trevor Noah", age: 34, city: "Johannesburg")
+Comedian.create(name: "Ali Wong", age: 36, city: "San Francisco, CA")
+Comedian.create(name: "Sarah Silverman", age: 48, city: "Bedford, NH")
+Comedian.create(name: "Ron White", age: 62, city: "Fritch, TX")
+Comedian.create(name: "Tig Notaro", age: 47, city: "Jackson, MS")
+Comedian.create(name: "Joan Rivers", age: 81, city: "Brooklyn, NY")

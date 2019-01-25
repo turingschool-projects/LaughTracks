fd7037c648d1a6c79bdcc1124947facc883add53
Ty Mazey <tymazey@gmail.com>
2019-01-25 10:20:30 -0700

    adds test for feature testing seeing a comedians specials

--- spec/features/comedian_index_spec.rb
+++ spec/features/comedian_index_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe "as a visitor", type: :feature do
       expect(page).to have_content('Atlanta')
     end
   end
+
+  it 'should show a comedians specials' do
+    comedian_1 = Comedian.create(name: 'Mitch Hedberg', age: 30, born: 'St. Paul', deceased: true)
+    special_1 = Special.create(name: "Comedy Central Presents", length: 30)
+
+    visit '/comedians'
+
+    within '#mitch-hedberg-specials'
+      expect(page).to have_content("Comedy Central Presents")
+      expect(page).to have_content("30 minutes")
+  end
 end

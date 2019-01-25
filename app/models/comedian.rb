d30e84d5040a771f9a52b18dc75f61bd29afb980
Ty Mazey <tymazey@gmail.com>
2019-01-25 10:33:14 -0700

    adds belongs to/ has many, dependancys in models for comedians and specials

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,6 +2,7 @@ class Comedian < ActiveRecord::Base
   validates :name, presence: true
   validates :age, presence: true
   validates :born, presence: true
+  has_many :specials
 
   def name_as_kebab
     name.downcase.gsub(/ /, "-")

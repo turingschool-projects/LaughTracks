8fd1bde467d206f3bad104100215b0533d08eb20
Scott Thomas <smthom05@gmail.com>
2019-01-25 12:38:26 -0700

    specials seed data entered

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_160858) do
+ActiveRecord::Schema.define(version: 2019_01_25_184903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2019_01_25_160858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.text "name"
+    t.integer "run_time"
+    t.text "image_url"
+    t.integer "comedian_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

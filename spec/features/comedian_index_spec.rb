965abacd7503cfa8615789edecbd9e990d325829
Ty Mazey <tymazey@gmail.com>
2019-01-25 11:01:43 -0700

    adds expect in test to look for image for specials

--- spec/features/comedian_index_spec.rb
+++ spec/features/comedian_index_spec.rb
@@ -23,13 +23,14 @@ RSpec.describe "as a visitor", type: :feature do
 
   it 'should show a comedians specials' do
     comedian_1 = Comedian.create(name: 'Mitch Hedberg', age: 30, born: 'St. Paul', deceased: true)
-    comedian_1.specials.create(name: "Comedy Central Presents", length: 30)
+    comedian_1.specials.create(name: "Comedy Central Presents", length: 30, image_url: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2018/06/gettyimages-74714565-h_2018.jpg")
 
     visit '/comedians'
 
     within '#mitch-hedberg-specials' do
       expect(page).to have_content("Comedy Central Presents")
       expect(page).to have_content("30 minutes")
+      expect(page).to have_content("https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2018/06/gettyimages-74714565-h_2018.jpg")
     end
   end
 end

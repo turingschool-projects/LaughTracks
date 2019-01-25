f5d12338f59d0a5a2a29e2ad1f40fc070e8f7774
plapicola <p.lapicola@gmail.com>
2019-01-24 20:53:34 -0700

    Adds tests for validation of runtime to special_spec

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -2,13 +2,27 @@ RSpec.describe Special do
   describe 'Validations' do
     describe 'Required Field(s)' do
       it 'should be invalid if missing a name' do
-        special = Special.create
+        special = Special.create(comedian_id: 1, runtime: 84)
         expect(special).to_not be_valid
       end
 
       it 'should be invalid if missing a comedian id' do
-        special = Special.create(name: "Cinco")
+        special = Special.create(name: "Cinco", runtime: 84))
         expect(special).to_not be_valid
+      end
+
+      it "should be invalid if missing a runtime" do
+        special = Special.create(name: "Cinco", comedian_id: 1)
+      end
+    end
+  end
+
+  describe 'class methods' do
+    describe 'average_runtime' do
+      it "can give an average of all specials" do
+        Special.create(name: "Cinco",  comedian_id: 1)
+        Special.create(name: "Cinco", comedian_id: 1)
+        Special.average_runtime
       end
     end
   end

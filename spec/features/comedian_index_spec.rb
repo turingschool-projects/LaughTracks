2e987da08f26edf4d860a753a9956f43034934a6
Ty Mazey <tymazey@gmail.com>
2019-01-25 09:11:34 -0700

    adds test to see list of comedians

--- spec/features/comedian_index_spec.rb
+++ spec/features/comedian_index_spec.rb
@@ -1,4 +1,12 @@
 RSpec.describe "as a visitor", type: :feature do
 
-  
+  it 'should show a list of comedians' do
+    comedian_1 = Comedian.create(name: 'Mitch Hedberg', age: 30, born: 'St. Paul')
+    comedian_2 = Comedian.create(name: 'Bill Burr', age: 59, born: 'Alanta')
+    visit '/comedians'
+
+    within '#comedian_list'
+    expect(page).to have_content('Mitch Hedberg')
+    expect(page).to have_content('Bill Burr')
+  end
 end

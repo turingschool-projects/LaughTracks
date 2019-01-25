4860a34779f9ea40ebc1ebaacee64716537cd01b
Ty Mazey <tymazey@gmail.com>
2019-01-25 10:31:22 -0700

    creates migration for specials db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_220849) do
+ActiveRecord::Schema.define(version: 2019_01_25_172816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2019_01_24_220849) do
     t.integer "age"
     t.text "born"
     t.boolean "deceased", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.text "name"
+    t.integer "length"
+    t.integer "comedian_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

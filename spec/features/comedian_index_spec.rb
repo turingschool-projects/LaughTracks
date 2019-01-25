b1d2c28d203514ea971f7918bf45919f67ed4d26
Ty Mazey <tymazey@gmail.com>
2019-01-25 09:25:23 -0700

    makkes comedian index html for feature test pass

--- spec/features/comedian_index_spec.rb
+++ spec/features/comedian_index_spec.rb
@@ -1,12 +1,19 @@
 RSpec.describe "as a visitor", type: :feature do
 
-  it 'should show a list of comedians' do
+  it 'should show a list of comedians and there attributes' do
     comedian_1 = Comedian.create(name: 'Mitch Hedberg', age: 30, born: 'St. Paul')
     comedian_2 = Comedian.create(name: 'Bill Burr', age: 59, born: 'Alanta')
     visit '/comedians'
+    save_and_open_page
 
-    within '#comedian_list'
-    expect(page).to have_content('Mitch Hedberg')
-    expect(page).to have_content('Bill Burr')
+    within '#comedian_list' do
+      expect(page).to have_content('Mitch Hedberg')
+      expect(page).to have_content('Bill Burr')
+    end
+
+    within '#comedian_attributes' do
+      expect(page).to have_content('30')
+      expect(page).to have_content('St. Paul')
+    end
   end
 end

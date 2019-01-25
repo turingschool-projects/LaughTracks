a960ebe57595625a5708c6fc422668a5668deb2b
Ty Mazey <tymazey@gmail.com>
2019-01-25 10:43:20 -0700

    adds second validation test for specials, requiring a coemdian id

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -2,8 +2,11 @@ RSpec.describe Special do
   describe 'Validations' do
     describe 'Required Field(s)' do
       it 'should be invalid if missing a name' do
-        special = Special.create
+        special = Special.create(comedian_id: 1)
         expect(special).to_not be_valid
+      end
+      it 'should be invalid if missing a comedian id' do
+        special = Special.create(name: "lol")
       end
     end
   end

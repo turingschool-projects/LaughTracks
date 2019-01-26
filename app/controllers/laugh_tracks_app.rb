17e9f4a97b93f14064cd732408ec91f89177c861
Scott Thomas <smthom05@gmail.com>
2019-01-26 11:44:01 -0700

    comedian specials names appear on page now

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,6 +2,7 @@ class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
     @comedians = Comedian.all
+    # @specials = Special.get_comedian_specials()
     erb :index
   end
 
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,7 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
+
+  def self.get_comedian_specials(comedian_id)
+    where(comedian_id: comedian_id)
+  end
 end

01dcdb5ab5ad1e09687a178b6a856fe7a7dc8731
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-25 22:54:46 -0700

    adds id for each comedian section

--- spec/features/user_sees_list_of_comedians_spec.rb
+++ spec/features/user_sees_list_of_comedians_spec.rb
@@ -3,18 +3,25 @@ RSpec.describe "as a visitor", type: :feature do
 
     it "should show all comedians" do
 
-      Comedian.create!(name: "Bob Comedian", age:40, city:"New York")
-      Comedian.create!(name: "Jenny Comedian", age:50, city:"LA")
-      Comedian.create!(name: "Juan Comedian", age:42, city:"Denver")
-      Comedian.create!(name: "Larry IDK", age:42, city:"New York")
-      Special.create!(name:"Very special", run_time: 144)
+      bob = Comedian.create!(name: "Bob Comedian", age:40, city:"New York")
+      jenny = Comedian.create!(name: "Jenny Comedian", age:50, city:"LA")
+      juan = Comedian.create!(name: "Juan Comedian", age:42, city:"Denver")
+      larry = Comedian.create!(name: "Larry IDK", age:42, city:"New York")
+      very = Special.create!(name:"Very special", run_time: 144)
 
       visit '/comedians'
       save_and_open_page
 
-      within '#page_title' do
-        expect(page).to have_content("Comedians")
+      within "#comedian-#{bob.id}" do
+        expect(page).to have_content("Bob Comedian")
+        expect(page).to have_content("40")
+        expect(page).to have_content("New York")
       end
-    end
+
+
+
   end
+
+end
+
 end

28aa2a3210758354f15950874b256b580036554f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-26 15:13:59 -0700

    Added average age method

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,4 +2,10 @@ class Comedian < ActiveRecord::Base
   has_many :specials
   validates_presence_of :name,
                         :age
+
+  def self.average_age
+    average(:age)
+  end
+
+  
 end

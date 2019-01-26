ddb0c124ecc320e47eeaede1c23454b235eb7a6b
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-25 22:37:49 -0700

    adds feature test for title

--- spec/features/user_sees_list_of_comedians_spec.rb
+++ spec/features/user_sees_list_of_comedians_spec.rb
@@ -1,13 +1,20 @@
 RSpec.describe "as a visitor", type: :feature do
+  describe "When I visit /comedians" do
 
-  xit "should show Comedians as title" do
-    visit '/comedians'
-    save_and_open_page
+    it "should show all comedians" do
 
-    within ('#jumbotron') do
+      Comedian.create!(name: "Bob Comedian", age:40, city:"New York")
+      Comedian.create!(name: "Jenny Comedian", age:50, city:"LA")
+      Comedian.create!(name: "Juan Comedian", age:42, city:"Denver")
+      Comedian.create!(name: "Larry IDK", age:42, city:"New York")
+      Special.create!(name:"Very special", run_time: 144)
+
+      visit '/comedians'
+      save_and_open_page
+
+      within '#page_title' do
         expect(page).to have_content("Comedians")
+      end
+    end
   end
-
-end
-
 end

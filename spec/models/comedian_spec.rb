dddd1fbdc3d3ce1ef60e38653f760d995cfdb89d
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-26 15:13:40 -0700

    Added test for average age

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe Comedian do
       end
     end
   end
+  describe 'Class Methods' do
+    before :each do
+      @comedian_1 = Comedian.create(name: 'Comedian 1', age: 31, city: 'New York')
+      @comedian_2 = Comedian.create(name: 'Comedian 2', age: 42, city: 'New York')
+      @comedian_3 = Comedian.create(name: 'Comedian 3', age: 53, city: 'Los Angeles')
+      @comics = [@comedian_1, @comedian_2, @comedian_3]
+    end
+    it 'should average all ages' do
+      expected = 42
+      actual = Comedian.average_age
+
+      expect(actual).to eq(expected)
+    end
+
+  end
 end

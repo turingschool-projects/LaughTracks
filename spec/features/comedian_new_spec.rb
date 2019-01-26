b9a0a17cc5110042f467a2da352dcbda185fff09
Ty Mazey <tymazey@gmail.com>
2019-01-25 17:12:35 -0700

    adds test for creating new comedian

--- spec/features/comedian_new_spec.rb
+++ spec/features/comedian_new_spec.rb
@@ -10,11 +10,23 @@ RSpec.describe "as a visitor", type: :feature do
       expect(page).to have_selector("input[value='Name']")
       expect(page).to have_selector("input[value='Age']")
       expect(page).to have_selector("input[value='Birthplace (city, state)']")
-      expect(page).to have_selector("input[value='Picture Url (optional)']")
+      expect(page).to have_selector("input[value='Picture URL (optional)']")
       expect(page).to have_button("Submit")
     end
 
     click_on "Submit"
     expect(current_path).to eq("/comedians")
+  end
+
+  it 'should create a new comedian in the database when filled out' do
+
+    visit 'comedians/new'
+    fill_in("Name", with: "Bob")
+    fill_in("Age", with: "37")
+    fill_in("Birthplace (city, state)", with: "Denver, Colorado")
+    fill_in("Picture URL", with: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2018/06/gettyimages-74714565-h_2018.jpg")
+    click_on "Submit"
+
+    expect(Comedian.count).to eq(1)
   end
 end

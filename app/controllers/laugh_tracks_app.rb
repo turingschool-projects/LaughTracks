7dd7f3eb41fbfc9103472c2ebf93bb67fda513ac
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-26 13:00:40 -0700

    more css styling

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -3,10 +3,10 @@ class LaughTracksApp < Sinatra::Base
   get '/' do
     redirect '/comedians'
   end
-  
+
   get '/comedians' do
     if params[:age]
-      @comedians = Comedian.where(age:params[:age].to_i)
+      @comedians = Comedian.where('age = ?', params[:age].to_i)
     else
       @comedians = Comedian.all
     end

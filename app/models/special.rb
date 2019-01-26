0e62a7350339f729d14f825bdc880eaea638c598
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-26 15:49:10 -0700

    Added average runtime class method

--- app/models/special.rb
+++ app/models/special.rb
@@ -2,4 +2,8 @@ class Special < ActiveRecord::Base
   belongs_to :comedian
   validates_presence_of :name,
                         :runtime
+
+  def self.average_runtime
+    average(:runtime).to_i
+  end
 end

e1714cff3d4b92c33e880c76a8a1877ff6545d85
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-26 14:12:15 -0700

    Added styling, updated views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 require_relative '../app/models/comedian'
+require_relative '../app/models/special'
 
 Comedian.destroy_all
 Special.destroy_all
 
+@img_1 = "https://m.media-amazon.com/images/M/MV5BZjgwZjQ5OTgtMWY0MC00YTQ5LWE2MzEtMWEyYTc5ODhlMTQyXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+@img_2 = "https://m.media-amazon.com/images/M/MV5BODU5MWI2YWEtNTQ3Ny00OWQ3LThkNTUtYzdiNDVjNDczM2NiXkEyXkFqcGdeQXVyMTk3NDAwMzI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+@img_3 = "https://m.media-amazon.com/images/M/MV5BMTQ0Mzc4MzkwOF5BMl5BanBnXkFtZTcwMjc1MTk5Mw@@._V1_.jpg"
+@comedian_1 = Comedian.create(name: 'Comedian 1', age: 31, city: 'New York')
+@comedian_2 = Comedian.create(name: 'Comedian 2', age: 42, city: 'New York')
+@comedian_3 = Comedian.create(name: 'Comedian 3', age: 53, city: 'Los Angeles')
+@comics = [@comedian_1, @comedian_2, @comedian_3]
 
-Comedian.create()
-Special.create()
+@special_1 = @comedian_1.specials.create(name: 'Special 1', runtime: 70, thumbnail: @img_1)
+@special_2 = @comedian_1.specials.create(name: 'Special 2', runtime: 70, thumbnail: @img_2)
+@special_3 = @comedian_2.specials.create(name: 'Special 3', runtime: 70, thumbnail: @img_3)

342126f069717f713b167dc20175a200c3b6f3bf
earl-stephens <stephensearl@hotmail.com>
2019-01-26 23:05:01 -0700

    updated controller for get comedians route to show both comedians by age query and comedians with no age query

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,11 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.find_comedians_by_age(params[:age])
+    if (params[:age]) == nil
+      @comedians = Comedian.all
+    else
+      @comedians = Comedian.find_comedians_by_age(params[:age])
+    end
     @specials = Special.all
     # require 'pry'; binding.pry
     erb :welcome

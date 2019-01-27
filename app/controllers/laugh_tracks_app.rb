ede54782458b4fd3c0fdceae8af033b6ed377f84
earl-stephens <stephensearl@hotmail.com>
2019-01-26 22:59:40 -0700

    updated controller for get comedians route to be able to select url queries

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,7 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.all
+    @comedians = Comedian.find_comedians_by_age(params[:age])
     @specials = Special.all
     # require 'pry'; binding.pry
     erb :welcome

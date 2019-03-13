b77f5922f292e5a37a05d0476b9d2047751a94bf
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-13 08:38:19 -0600

    Creates models - adds  method to comedian model

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,7 @@
 class Comedian < ActiveRecord::Base
+  # has_many :specials
+  def specials
+    Special.where(comedian_id: id)
+  end
+
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,2 +1,3 @@
 class Special < ActiveRecord::Base
+
 end

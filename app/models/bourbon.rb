7516ed203e5520a8766ad81c67a4bcb0bf6cb380
Carrie Walsh <carriewalsh@gmail.com>
2019-03-13 16:16:21 -0600

    Adds bourbon model initialize and all

--- app/models/bourbon.rb
+++ app/models/bourbon.rb
@@ -1,2 +1,24 @@
 class Bourbon < ActiveRecord::Base
+  attr_reader   :name,
+                :distillery,
+                :location,
+                :description,
+                :proof,
+                :expert_score,
+                :avg_rating,
+                :bottle_image
+  def initialize(bourbon_params)
+    @name = bourbon_params["name"]
+    @distillery = bourbon_params["distillery"]
+    @location = bourbon_params["location"]
+    @description = bourbon_params["description"]
+    @proof = bourbon_params["proof"]
+    @expert_score = bourbon_params["expert_score"]
+    @avg_rating = bourbon_params["avg_rating"]
+    @bottle_image = bourbon_params["bottle_image"]
+  end
+
+  def self.all
+    bourbons = Bourbon.all
+  end
 end

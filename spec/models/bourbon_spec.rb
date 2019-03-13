890eff050bfb4047d63ef117cb74bf56969f1022
Carrie Walsh <carriewalsh@gmail.com>
2019-03-13 16:07:26 -0600

    Adds model tests for bourbon

--- spec/models/bourbon_spec.rb
+++ spec/models/bourbon_spec.rb
@@ -1,12 +1,42 @@
 RSpec.describe Bourbon do
   describe 'Validations' do
     describe 'Required Field(s)' do
+      it 'should be invalid if missing a name' do
+        bottle = Bourbon.create(location: 'Frankfort, KY')
+        expect(bottle).to_not be_valid
+      end
+
+      it 'should be invalid if missing a distillery' do
+        bottle = Bourbon.create(location: 'Frankfort, KY')
+        expect(bottle).to_not be_valid
+      end
+
       it 'should be invalid if missing a location' do
         bottle = Bourbon.create(distillery: "Buffalo Trace")
         expect(bottle).to_not be_valid
       end
 
-      it 'should be invalid if missing a distillery' do
+      it 'should be invalid if missing a description' do
+        bottle = Bourbon.create(location: 'Frankfort, KY')
+        expect(bottle).to_not be_valid
+      end
+
+      it 'should be invalid if missing a proof' do
+        bottle = Bourbon.create(location: 'Frankfort, KY')
+        expect(bottle).to_not be_valid
+      end
+
+      it 'should be invalid if missing an expert score' do
+        bottle = Bourbon.create(location: 'Frankfort, KY')
+        expect(bottle).to_not be_valid
+      end
+
+      it 'should be invalid if missing an average rating' do
+        bottle = Bourbon.create(location: 'Frankfort, KY')
+        expect(bottle).to_not be_valid
+      end
+
+      it 'should be invalid if missing a bottle image' do
         bottle = Bourbon.create(location: 'Frankfort, KY')
         expect(bottle).to_not be_valid
       end

4954107e72b33a11bc80909d06707df7f21b29e5
Carrie Walsh <carriewalsh@gmail.com>
2019-03-13 09:25:11 -0600

    Adds first get to controller

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,10 @@
+require_relative "../models/bourbon"
+
 class LaughTracksApp < Sinatra::Base
+  set :root, FIle.expand_path("..", __dir__)
+
+  get "/bourbons" do
+    erb :dashboard
+  end
+  
 end

20d020bc6c70d54864708e4be5bb0504054b2007
Blake <blake.enyart@gmail.com>
2019-03-13 17:50:07 -0600

    Reorganized file structure with public

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,16 @@ require './app/models/comedian'
 require './app/models/tv_special'
 
 file_path = './db/comedians.csv'
-comedians_list = CSV.new(File.open(file_path), headers: true, header_converters: :symbol).read
+file = File.open(file_path)
+comedians_list = CSV.new(file, headers: true, header_converters: :symbol).read
 
 comedians_list.each do |comedian|
   Comedian.create(name: comedian[:name], age: comedian[:age], city: comedian[:hometown])
 end
 
-
 file_path = './db/tv_special.csv'
-tv_special_list = CSV.new(File.open(file_path), headers: true, header_converters: :symbol).read
+file = File.open(file_path)
+tv_special_list = CSV.new(file, headers: true, header_converters: :symbol).read
 
 tv_special_list.each do |tv_special|
   TVSpecial.create(name: tv_special[:name], age: tv_special[:age], city: tv_special[:hometown])

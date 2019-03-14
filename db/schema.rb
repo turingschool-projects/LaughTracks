e70a00ececa2ee8597694d5f34bd46d24cb0e408
Chi Tran <chidtran@gmail.com>
2019-03-13 21:57:48 -0600

    Adds Comedians to Specials migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_031724) do
+ActiveRecord::Schema.define(version: 2019_03_14_034605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_031724) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comedian_id"
   end
 
 end

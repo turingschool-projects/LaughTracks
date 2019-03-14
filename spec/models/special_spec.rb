692f5dc8fae6590e166f02e32e416a323b253164
Matt Levy <milevy1@gmail.com>
2019-03-14 16:23:01 -0600

    Added specials spec validation for name, runtime, thumbnail_link

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -2,7 +2,23 @@ RSpec.describe Special do
   describe 'Validations' do
     describe 'Required Field(s)' do
       it 'should be invalid if missing a name' do
-        special = Special.create
+        special = Special.create(runtime: 71, 
+                                 thumbnail_link: '1.jpg')
+
+        expect(special).to_not be_valid
+      end
+
+      it 'should be invalid if missing a runtime' do
+        special = Special.create(name: 'Tom Segura: Disgraceful',
+                                 thumbnail_link: '1.jpg')
+
+        expect(special).to_not be_valid
+      end
+
+      it 'should be invalid if missing a thumbnail_link' do
+        special = Special.create(name: 'Tom Segura: Disgraceful',
+                                 runtime: 71)
+
         expect(special).to_not be_valid
       end
     end

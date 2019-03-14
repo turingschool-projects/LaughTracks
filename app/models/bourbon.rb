67aaafb33ee2cbf57126d80c2321658c36dc196f
Carrie Walsh <carriewalsh@gmail.com>
2019-03-13 21:22:51 -0600

    Removes init/attr from model, creates layout, un-comments seeds

--- app/models/bourbon.rb
+++ app/models/bourbon.rb
@@ -1,27 +1,7 @@
 class Bourbon < ActiveRecord::Base
   # has_many :awards
 
-  attr_reader   :name,
-                :distillery,
-                :location,
-                :description,
-                :proof,
-                :expert_score,
-                :avg_rating,
-                :bottle_image
-
-  def initialize(bourbon_params)
-    @name = bourbon_params["name"]
-    @distillery = bourbon_params["distillery"]
-    @location = bourbon_params["location"]
-    @description = bourbon_params["description"]
-    @proof = bourbon_params["proof"]
-    @expert_score = bourbon_params["expert_score"]
-    @avg_rating = bourbon_params["avg_rating"]
-    @bottle_image = bourbon_params["bottle_image"]
-  end
-
-  def self.all
-    
-  end
+  # def self.state
+  #   bourbons =
+  # end
 end

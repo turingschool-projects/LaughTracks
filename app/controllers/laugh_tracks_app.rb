24dc75125809e4bc4b631a42af43cc2a8d7279d6
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-03-14 11:34:20 -0600

    Adding initial file commits

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,11 @@
 class LaughTracksApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :public_folder, File.dirname(__FILE__) + '../public'
+
+  get '/' do
+    @comedians = Comedian.all
+    @specials = Special.all
+    erb :comedians
+  end
+
 end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,10 @@
 class Comedian < ActiveRecord::Base
+  has_many :specials
+
+  def average_age
+    @comedians.sum do |comedian|
+      comedian.age
+    end 
+  end
+
 end

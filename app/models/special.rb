5eb79a176f044ec77cbddcce114c444460a63586
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-14 16:04:57 -0600

    adds validations and tests to special.rb

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,6 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
+  validates_presence_of :name,
+                        :runtime_mins,
+                        :image_url
 end
--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -1,10 +1,20 @@
 RSpec.describe Special do
   describe 'Validations' do
-    describe 'Required Field(s)' do
-      it 'should be invalid if missing a name' do
-        special = Special.create
-        expect(special).to_not be_valid
-      end
+    it 'should be invalid if missing a name' do
+      special = Special.new(runtime_mins: 77, image_url: 'dostuff')
+
+      expect(special).to_not be_valid
+    end
+
+    it 'should be invalid if missing a runtime' do
+      special = Special.new(name: 'special', image_url: 'dostuff')
+
+      expect(special).to_not be_valid
+    end
+    it 'should be invalid if missing a image_url' do
+      special = Special.new(name: 'special', runtime_mins: 77)
+
+      expect(special).to_not be_valid
     end
   end
 end

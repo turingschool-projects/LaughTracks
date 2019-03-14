1cdf22446b3350800630fb448b5eb2ac834ca61e
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-03-14 15:58:43 -0600

    Passed tests to average runtime

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,10 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
+
+  validates :name, presence: true
+
+  def self.average_runtime
+    Special.average(:runtime).to_f.round(2)
+  end
+
 end

b1da6d433a397424aece4cfa759c652ba6b0fbb2
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-14 16:29:28 -0600

    Adds relationships text section

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Comedian do
       end
     end
   end
+
+  describe 'Relationships' do
+    it 'has many specials' do
+      c1 = Comedian.create(name: "Adam Sandler",	age: 52,	home_town: "New York, NY",	photo: "https://cdn1.thr.com/sites/default/files/2012/12/6_degrees_of_judd_apatow_adam_sandler.jpg")
+
+      c1.specials.create(name: "100% Fresh",	run_time: 73, thumbnail: "https://m.media-amazon.com/images/M/MV5BNWNiYWJlNzQtYmMwMS00MjUyLWE4YjAtNDVjZWUwNzBjZDQ1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
+      c1.specials.create(name: "The Comeback Kid", run_time: 61, thumbnail: "https://m.media-amazon.com/images/M/MV5BMDQ3NjU0NmQtYjgyZS00MzIzLWJjNDEtMWY5YjczYjc0MTMyXkEyXkFqcGdeQXVyMjI0MjUyNTc@._V1_UY268_CR4,0,182,268_AL_.jpg")
+
+      expect(c1.specials.length).to eq(2)
+    end
+  end
 end

f250269aca1df28f41db1d318bdfe9840794a719
Matt Weiss <weiss.matt@outlook.com>
2019-03-13 23:03:31 -0600

    add ivars for db access

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,6 +2,8 @@ class LaughTracksApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/comedians' do
+    @comedians = Comedian.all
+    @specials = Special.all
     erb:index
   end
 end

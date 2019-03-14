48b7a8f97130dcfa2639d6f1df497404b6674dd4
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-13 19:14:52 -0600

    Adds model methods

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,7 +1,27 @@
 class Comedian < ActiveRecord::Base
-  # has_many :specials
-  def specials
-    Special.where(comedian_id: id)
+  has_many :specials
+
+  def self.average_age
+    comedians = Comedian.all
+    combined_age = comedians.sum do |comedian|
+      comedian.age
+    end
+      combined_age / comedians.count
   end
 
+  def self.hometowns
+    comedians = Comedian.all
+    towns_string = ""
+    all_towns(comedians).each do |town|
+      towns_string << town << " - "
+    end
+    towns_string
+  end
+
+  def self.all_towns(comedians)
+    all_towns = comedians.map do |comedian|
+      comedian.home_town.to_s
+    end
+    all_towns.sort.uniq
+  end
 end
--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,12 @@
 class Special < ActiveRecord::Base
+  belongs_to :comedians
+
+  def self.average_run_time
+    specials = Special.all
+    combined_length = specials.sum do |special|
+      special.run_time
+    end
+      combined_length / specials.count
+  end
 
 end

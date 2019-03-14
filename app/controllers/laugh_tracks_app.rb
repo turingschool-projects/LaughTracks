3159e63c584d256f736bbebdc2bacf25f25d99ba
Carrie Walsh <carriewalsh@gmail.com>
2019-03-14 15:16:07 -0600

    Adds new controller methods for sort and select

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -11,4 +11,46 @@ class LaughTracksApp < Sinatra::Base
     erb :index
   end
 
+  get "/bourbons/new" do
+    erb :new
+  end
+
+# is this right?
+  post "/bourbons" do
+    bourbon = Bourbon.create(bourbon_params)
+    redirect: "/bourbons"
+  end
+
+#so now my @bourbons is sorted, right?
+  get "/bourbons?sortby=name" do
+    @bourbons = Bourbon.sort_name
+    erb :index
+  end
+
+  get "bourbons?sortby=distillery" do
+    @bourbons = Bourbon.sort_distillery
+    erb :index
+  end
+
+  get "bourbons?sortby=proof" do
+    @bourbons = Bourbon.sort_proof
+    erb :index
+  end
+
+  get "bourbons?sortby=expert_score" do
+    @bourbons = Bourbon.sort_expert_score
+    erb :index
+  end
+
+  get "bourbons?sortby=avg_rating" do
+    @bourbons = Bourbon.sort_avg_rating
+    erb :index
+  end
+
+  # no idea if this will work because it depends on the states available
+  get "bourbons?select=#{state}" do
+    @bourbons = Bourbon.select_state(state)
+    erb :index
+  end
+
 end

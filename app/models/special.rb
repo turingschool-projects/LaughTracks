01a92a584562f408970177432dc9983050759b85
Matt Weiss <weiss.matt@outlook.com>
2019-03-14 17:54:21 -0600

    add abbreviation method

--- app/models/special.rb
+++ app/models/special.rb
@@ -1,3 +1,9 @@
 class Special < ActiveRecord::Base
   belongs_to :comedian
+
+  validates_presence_of :name, :runtime, :comedian_id
+
+  def abbreviation
+    name.downcase.slice(0,5).squeeze
+  end
 end

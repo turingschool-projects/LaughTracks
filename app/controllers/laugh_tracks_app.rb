fcf69048282d15ff2ec932dff6abd3201dae742a
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-03-14 15:56:49 -0600

    Updated file path from / to /comedians

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,7 +2,7 @@ class LaughTracksApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   set :public_folder, File.dirname(__FILE__) + '../public'
 
-  get '/' do
+  get '/comedians' do
     @comedians = Comedian.all
     @specials = Special.all
     erb :comedians

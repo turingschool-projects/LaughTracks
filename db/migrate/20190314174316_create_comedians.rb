083e538ab27906e4c0de7abd8d634f15b00f53e4
Rob Stringer <stringer525@gmail.com>
2019-03-14 12:22:03 -0600

    CreateComedians table attributes created

--- db/migrate/20190314174316_create_comedians.rb
+++ db/migrate/20190314174316_create_comedians.rb
@@ -1,4 +1,13 @@
 class CreateComedians < ActiveRecord::Migration[5.2]
   def change
+    create_table :comedians do |t|
+      t.integer :comedian_id
+      t.text    :name
+      t.integer :age
+      t.text :city
+      
+
+      t.timestamps
+    end
   end
 end

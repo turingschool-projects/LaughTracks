329d92ff09e71a5c34b5e159bacf53de8a120abc
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-03-14 15:17:16 -0600

    ran migration on specials table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_210940) do
+ActiveRecord::Schema.define(version: 2019_03_14_211400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_03_14_210940) do
     t.text "name"
     t.integer "age"
     t.text "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.text "name"
+    t.integer "runtime"
+    t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

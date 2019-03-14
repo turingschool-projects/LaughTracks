ec57245bfc9b58acc67827fb8dc3cc83eb3b0ca5
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-13 20:55:12 -0600

    post migration commit

--- db/migrate/20190314024747_create_comedians_table.rb
+++ db/migrate/20190314024747_create_comedians_table.rb
@@ -7,5 +7,6 @@ class CreateComediansTable < ActiveRecord::Migration[5.2]
       t.text :image_url
 
       t.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_020824) do
+ActiveRecord::Schema.define(version: 2019_03_14_024747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comedians", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.text "birthplace"
+    t.text "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "specials", force: :cascade do |t|
     t.text "name"

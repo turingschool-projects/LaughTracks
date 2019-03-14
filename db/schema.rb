2857311fff2bda47f3a0d287adf0c6eac32b18d8
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-03-14 15:25:16 -0600

    added comedian_id foreign key to specials table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_211400) do
+ActiveRecord::Schema.define(version: 2019_03_14_212055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_211400) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comedian_id"
   end
 
 end

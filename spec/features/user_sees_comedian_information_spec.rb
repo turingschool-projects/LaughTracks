c2b0f7ec5fcf670d6be406cfc92556c11533a4cd
Matt Levy <milevy1@gmail.com>
2019-03-14 14:12:02 -0600

    Added feature test user sees comedian information

---
+++ spec/features/user_sees_comedian_information_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'a user visits comedians page' do
+  context 'they visit /comedians' do
+    it 'should show comedian information' do
+      Comedian.create(name: 'Tom Segura',
+                      age: 39,
+                      hometown: 'Cincinnati',
+                      headshot_link: 'http://flapperscomedy.com/med/tomseguaheadshot.jpg')
+      visit '/comedians'
+
+      expect(page).to have_content('Tom Segura')
+      expect(page).to have_content('39')
+      expect(page).to have_content('Cincinnati')
+    end
+  end
+end

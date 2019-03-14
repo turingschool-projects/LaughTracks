005259144b22308a3f35a430e8a6afe2b988efaf
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-14 16:04:23 -0600

    adds validations and tests to comedian

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,17 +1,17 @@
 RSpec.describe Comedian do
   describe 'Validations' do
     it 'should be invalid if missing a name' do
-      comic = Comedian.create(age: 48, birthplace: 'Seattle')
+      comic = Comedian.new(age: 48, birthplace: 'Seattle')
       expect(comic).to_not be_valid
     end
 
     it 'should be invalid if missing an age' do
-      comic = Comedian.create(name: 'Bill Burr', birthplace: 'Canton, MA')
+      comic = Comedian.new(name: 'Bill Burr', birthplace: 'Canton, MA')
       expect(comic).to_not be_valid
     end
 
     it 'should ge invalid if missing a city' do
-      comic = Comedian.create(name: 'Bill Burr', age: 50)
+      comic = Comedian.new(name: 'Bill Burr', age: 50)
 
       expect(comic).to_not be_valid
     end

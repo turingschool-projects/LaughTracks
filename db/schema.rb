0e47059074ab26a068c61903c7cebdc7c62047b8
Chi Tran <chidtran@gmail.com>
2019-03-13 21:35:05 -0600

    Creates and migrates Specials db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_234322) do
+ActiveRecord::Schema.define(version: 2019_03_14_031724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_03_12_234322) do
     t.text "name"
     t.integer "age"
     t.text "city"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specials", force: :cascade do |t|
+    t.text "title"
+    t.integer "length"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

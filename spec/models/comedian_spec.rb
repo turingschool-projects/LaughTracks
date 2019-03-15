ac1f91e0d02cdb200614524edeb10ef2269a3242
Matt Levy <milevy1@gmail.com>
2019-03-15 09:43:16 -0600

    Added spec for .all_hometowns method

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -32,5 +32,23 @@ RSpec.describe Comedian do
         expect(Comedian.average_age).to eq(46.5)
       end
     end
+
+    describe '.all_hometowns' do
+      it 'returns array of all hometowns' do
+        Comedian.create(name: 'Mitch Hedberg', age: 48, hometown: 'St. Paul', headshot_link: '1.jpg')
+        Comedian.create(name: 'Dave Chappelle', age: 45, hometown: 'Washington DC', headshot_link: '2.jpg')
+
+        expect(Comedian.all_hometowns).to eq(['St. Paul', 'Washington DC'])
+      end
+
+      it 'returns array of all hometowns with duplicates removed' do
+        Comedian.create(name: 'Mitch Hedberg', age: 48, hometown: 'St. Paul', headshot_link: '1.jpg')
+        Comedian.create(name: 'Dave Chappelle', age: 45, hometown: 'Washington DC', headshot_link: '2.jpg')
+        Comedian.create(name: 'Hannibal Buress', age: 36, hometown: 'Chicago', headshot_link: '3.jpg')
+        Comedian.create(name: 'John Mulaney', age: 36, hometown: 'Chicago', headshot_link: '4.jpg')
+
+        expect(Comedian.all_hometowns).to eq(['St. Paul', 'Washington DC', 'Chicago'])
+      end
+    end
   end
 end

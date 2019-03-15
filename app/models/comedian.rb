4c8d09a025f4bc98702393d1789c339bc5cfaf23
Matt Levy <milevy1@gmail.com>
2019-03-15 09:29:36 -0600

    Added .average_age method

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,4 +2,8 @@ class Comedian < ActiveRecord::Base
   has_many :specials
 
   validates_presence_of :name, :age, :hometown, :headshot_link
+
+  def self.average_age
+    sum(:age) / count.to_f
+  end
 end

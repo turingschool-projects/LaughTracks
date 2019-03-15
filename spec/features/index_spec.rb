f50e6f044f5d258316cd02f1a20941698d9b5bc8
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-14 20:56:40 -0600

    Improves testing against duplicate hometowns

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -65,20 +65,19 @@ RSpec.describe 'comedians homepage view' do
       it 'should see the average for all comedian ages and specials run time, along with a unique list of hometowns' do
         c1 = Comedian.create(name: "Adam Sandler",	age: 52,	home_town: "New York, NY",	photo: "https://cdn1.thr.com/sites/default/files/2012/12/6_degrees_of_judd_apatow_adam_sandler.jpg")
         c2 = Comedian.create(name: "John Mulaney",	age: 36,	home_town: "Chicago, IL", photo: "https://resizing.flixster.com/zOSxtyjb51wl7o4ceVADrTui2IY=/874x779/v1.cjs0MzU5NTtqOzE3OTgzOzEyMDA7ODc0Ozc3OQ")
-        c3 = Comedian.create(name: "Dave Chappelle",	age: 45,	home_town: "Washington D.C.",	photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQzMzA3MzU5NDg4MzIxMDk2/dave-chappelle_gettyimages-481295252jpg.jpg")
+        c10 = Comedian.create(name: "Hannibal Buress",	age: 36,	home_town: "Chicago, IL",	photo: "https://pmcvariety.files.wordpress.com/2018/03/hannibal-buress.jpg?w=1000")
 
         c1.specials.create(name: "100% Fresh",	run_time: 73, thumbnail: "https://m.media-amazon.com/images/M/MV5BNWNiYWJlNzQtYmMwMS00MjUyLWE4YjAtNDVjZWUwNzBjZDQ1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
         c2.specials.create(name: "The Comeback Kid", run_time: 61, thumbnail: "https://m.media-amazon.com/images/M/MV5BMDQ3NjU0NmQtYjgyZS00MzIzLWJjNDEtMWY5YjczYjc0MTMyXkEyXkFqcGdeQXVyMjI0MjUyNTc@._V1_UY268_CR4,0,182,268_AL_.jpg")
-        c3.specials.create(name: "Killin' Them Softly",	run_time: 60, thumbnail: "https://m.media-amazon.com/images/M/MV5BMTcyOGMxN2MtYzUwYy00YzQxLTgwZTItZjAxZTIzYWM1YzEyXkEyXkFqcGdeQXVyNjExODE1MDc@._V1_.jpg")
+        c10.specials.create(name: "Comedy Camisado",	run_time: 66, thumbnail: "https://m.media-amazon.com/images/M/MV5BNDQ0ZDA3NTQtNzI3NC00Y2NiLWI0ZTctN2RjNDYxNWEyODY4XkEyXkFqcGdeQXVyNTQwMDk4NDM@._V1_UX182_CR0,0,182,268_AL_.jpg")
 
         visit '/comedians'
 
         within '#statistics' do
-          expect(page).to have_content("44")
-          expect(page).to have_content("65")
+          expect(page).to have_content("41")
+          expect(page).to have_content("67")
           expect(page).to have_content("New York, NY")
           expect(page).to have_content("Chicago, IL")
-          expect(page).to have_content("Washington D.C.")
         end
       end
     end

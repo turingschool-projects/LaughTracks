547d247df8fa9e2be94662fa553e6eb018c1ef98
Rob Stringer <stringer525@gmail.com>
2019-03-14 19:24:29 -0600

    Added controller array of specials grouped by comedian

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -4,6 +4,9 @@ class LaughTracksApp < Sinatra::Base
     get '/comedians' do 
         @comedians = Comedian.all
         @specials = Special.all
+        @comedian_specials = Comedian.all.map do |comedian|
+            Special.where(comedian_id: comedian.comedian_id)
+        end  
         erb :comedians
     end
 end

e5a23a2a12ec0b2d810ba13dbfbae799241ac627
Matt Levy <milevy1@gmail.com>
2019-03-15 21:09:38 -0600

    Added controller to filter comedians by age

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,7 +1,11 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.all
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+    else
+      @comedians = Comedian.all
+    end
     erb :comedians
   end
 

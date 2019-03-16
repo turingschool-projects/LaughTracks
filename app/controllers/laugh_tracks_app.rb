66de26049cd7e932e95c64d85d259188727a3aaf
David Hardy <davehardy632@gmail.com>
2019-03-16 17:39:11 -0600

    Completed funtionality for all user stories except 7

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,13 @@
 class LaughTracksApp < Sinatra::Base
+  set :public_folder, File.dirname(__FILE__) + '../public'
+
+  get '/comedians' do
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+    else
+      @comedians = Comedian.all
+    end
+      @specials = Special.all
+      erb :"comedians/index"
+  end
 end

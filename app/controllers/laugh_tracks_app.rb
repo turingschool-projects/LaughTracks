5da0ac409e46506fcde24b6181fcb194ecc2043d
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-15 19:04:16 -0600

    Includes sorting validation in controller - possibly needs to be moved to models

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,8 +2,26 @@ class LaughTracksApp < Sinatra::Base
 
   get "/comedians" do
     @comedians = Comedian.all
-    @specials = Special.all
+    if @comedians.pluck(:age).include?(params[:age].to_i)
+      @comedians = Comedian.where(age: params[:age])
+      @specials = Special.where(comedian_id: @comedians.pluck(:id))
+    else
+      @comedians = Comedian.all
+      @specials = Special.all
+    end
     erb :index
   end
+
+  get "/comedians/new" do
+    erb :new
+  end
+  #
+  # post "/comedians" do
+  #
+  #   erb :comedians
+  # end
+
+
+
 
 end

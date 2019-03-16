5bb0794bb158c95605464bf7ca992b933f8c175d
Matt Levy <milevy1@gmail.com>
2019-03-15 21:26:01 -0600

    Added .specials_count method spec

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -72,5 +72,17 @@ RSpec.describe Comedian do
         expect(Comedian.all_hometowns_list).to eq('St. Paul, Washington DC')
       end
     end
+
+    describe '.specials_count' do
+      it 'returns count of all comedians specials' do
+        tom = Comedian.create(name: 'Tom Segura', age: 39, hometown: 'Cincinnati', headshot_link: 'http://flapperscomedy.com/med/tomseguaheadshot.jpg')
+        tom.specials.create(name: 'Tom Segura: Disgraceful', runtime: 71, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMGUyNTBiZjktMjk5Ni00Yzk3LTlkZTUtY2FiOTJiOGRhZWY3XkEyXkFqcGdeQXVyMTk2ODU0OTM@._V1_.jpg')
+        tom.specials.create(name: 'Tom Segura: Mostly Stories', runtime: 73, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BZWE2Nzc5N2YtOWM0OS00ZWVmLThhOTctNjNlNGI0NTQzMTU5XkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg')
+        mitch = Comedian.create(name: 'Mitch Hedberg', age: 37, hometown: 'St. Paul', headshot_link: 'https://upload.wikimedia.org/wikipedia/en/d/d5/Mitch_Hedberg.jpg')
+        mitch.specials.create(name: 'Comedy Central: Mitch Hedberg', runtime: 23, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMTQ0NDAyNDg5OV5BMl5BanBnXkFtZTgwMDUxNjEyMjE@._V1_.jpg')
+
+        expect(Comedian.specials_count).to eq(3)
+      end
+    end
   end
 end

88f2be458996fc46a5ea14cb27c7424f25744218
Matt Levy <milevy1@gmail.com>
2019-03-16 08:19:18 -0600

    Added specials to spec

--- spec/features/user_sees_comedians_filtered_by_age_spec.rb
+++ spec/features/user_sees_comedians_filtered_by_age_spec.rb
@@ -1,15 +1,13 @@
 RSpec.describe 'a user visits comedians page with age criteria' do
   context 'they visit /comedians?age=34' do
     before(:each) do
-      Comedian.create(name: 'Tom Segura',
-                      age: 39,
-                      hometown: 'Cincinnati',
-                      headshot_link: 'http://flapperscomedy.com/med/tomseguaheadshot.jpg')
+      c1 = Comedian.create(name: 'Tom Segura', age: 39, hometown: 'Cincinnati', headshot_link: 'http://flapperscomedy.com/med/tomseguaheadshot.jpg')
+      c1.specials.create(name: 'Tom Segura: Disgraceful', runtime: 71, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMGUyNTBiZjktMjk5Ni00Yzk3LTlkZTUtY2FiOTJiOGRhZWY3XkEyXkFqcGdeQXVyMTk2ODU0OTM@._V1_.jpg')
+      c1.specials.create(name: 'Tom Segura: Mostly Stories', runtime: 73, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BZWE2Nzc5N2YtOWM0OS00ZWVmLThhOTctNjNlNGI0NTQzMTU5XkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg')
 
-      Comedian.create(name: 'Mitch Hedberg',
-                      age: 34,
-                      hometown: 'St. Paul',
-                      headshot_link: 'https://upload.wikimedia.org/wikipedia/en/d/d5/Mitch_Hedberg.jpg')
+      c2 = Comedian.create(name: 'Mitch Hedberg', age: 34, hometown: 'St. Paul', headshot_link: 'https://upload.wikimedia.org/wikipedia/en/d/d5/Mitch_Hedberg.jpg')
+      c2.specials.create(name: 'Comedy Central: Mitch Hedberg', runtime: 28, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMTQ0NDAyNDg5OV5BMl5BanBnXkFtZTgwMDUxNjEyMjE@._V1_.jpg')
+      c2.specials.create(name: 'Just for Laughs', runtime: 30, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMTc4NzI1NzY4N15BMl5BanBnXkFtZTcwMjUwNDY3Mg@@._V1_.jpg')
 
       visit '/comedians?age=34'
     end

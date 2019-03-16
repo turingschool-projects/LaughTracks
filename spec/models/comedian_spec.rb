80b4458e7de572b3aa8040e154e6f4230dd3a6ea
Matt Levy <milevy1@gmail.com>
2019-03-16 11:25:54 -0600

    Moved spec specials .count and .average_runtime to special_spec

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -33,17 +33,6 @@ RSpec.describe Comedian do
       end
     end
 
-    describe '.average_specials_runtime' do
-      it 'returns average run length of every TV special on the page' do
-        c1 = Comedian.create(name: 'Mitch Hedberg', age: 48, hometown: 'St. Paul', headshot_link: '1.jpg')
-        c1.specials.create(name: 'Comedy Central: Mitch Hedberg', runtime: 10, thumbnail_link: '1.jpg')
-        c2 = Comedian.create(name: 'Dave Chappelle', age: 45, hometown: 'Washington DC', headshot_link: '2.jpg')
-        c2.specials.create(name: 'Dave Chappelle: Equanimity', runtime: 20, thumbnail_link: '2.jpg')
-
-        expect(Comedian.average_specials_runtime).to eq(15)
-      end
-    end
-
     describe '.all_hometowns' do
       it 'returns array of all hometowns' do
         Comedian.create(name: 'Mitch Hedberg', age: 48, hometown: 'St. Paul', headshot_link: '1.jpg')
@@ -70,18 +59,6 @@ RSpec.describe Comedian do
         Comedian.create(name: 'Dave Chappelle', age: 45, hometown: 'Washington DC', headshot_link: '2.jpg')
 
         expect(Comedian.all_hometowns_list).to eq('St. Paul, Washington DC')
-      end
-    end
-
-    describe '.specials_count' do
-      it 'returns count of all comedians specials' do
-        tom = Comedian.create(name: 'Tom Segura', age: 39, hometown: 'Cincinnati', headshot_link: 'http://flapperscomedy.com/med/tomseguaheadshot.jpg')
-        tom.specials.create(name: 'Tom Segura: Disgraceful', runtime: 71, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMGUyNTBiZjktMjk5Ni00Yzk3LTlkZTUtY2FiOTJiOGRhZWY3XkEyXkFqcGdeQXVyMTk2ODU0OTM@._V1_.jpg')
-        tom.specials.create(name: 'Tom Segura: Mostly Stories', runtime: 73, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BZWE2Nzc5N2YtOWM0OS00ZWVmLThhOTctNjNlNGI0NTQzMTU5XkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg')
-        mitch = Comedian.create(name: 'Mitch Hedberg', age: 37, hometown: 'St. Paul', headshot_link: 'https://upload.wikimedia.org/wikipedia/en/d/d5/Mitch_Hedberg.jpg')
-        mitch.specials.create(name: 'Comedy Central: Mitch Hedberg', runtime: 23, thumbnail_link: 'https://m.media-amazon.com/images/M/MV5BMTQ0NDAyNDg5OV5BMl5BanBnXkFtZTgwMDUxNjEyMjE@._V1_.jpg')
-
-        expect(Comedian.specials_count).to eq(3)
       end
     end
   end

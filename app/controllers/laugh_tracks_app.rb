3061f459f4fb6dc377a583b129a17db751023ca2
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-16 16:50:22 -0600

    adds ability to filter by age in url

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,8 +1,18 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
-    @comedians = Comedian.all
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+    else
+      @comedians = Comedian.all
+    end
     @specials = Special.all
     erb :comedians
   end
+
+  # get '/comedians/:age_filter' do
+  #   if params[:age_filter]
+  #   @comedians = Comedian.where(age: params[:age_filter])
+  #   erb :comedians
+  # end
 end

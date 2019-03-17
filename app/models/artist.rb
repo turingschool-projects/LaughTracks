36bd4d8354bf54fd2abe7c0e75c820d27af922bc
Jon Peterson <jon@pnwebs.com>
2019-03-16 20:45:54 -0600

    Pass test for photo filename

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ActiveRecord::Base
   validates :name, :formation_year, :origin, :album_count, presence: true
   has_many :albums
+
+  def photo
+    name.downcase.gsub(" ", "_")
+  end
 end

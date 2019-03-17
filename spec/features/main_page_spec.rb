77ae6847ba13864064633bfeebc9e12011e4c8be
Carrie Walsh <carriewalsh@gmail.com>
2019-03-17 14:28:35 -0600

    Adds feature tests for bourbon-cards

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe "a user visiting the main page" do
   before :each do
-    @b1 = Bourbon.create(name: "Bourbon", distillery: "Buffalo Trace" ,location: "Frankfort, KY" ,description: "tasty" ,proof: 90,expert_score: 91, avg_rating: 3.80,bottle_image: "http://")
+    @b1 = Bourbon.create(name: "Bourbon", distillery: "Buffalo Trace" ,location: "Frankfort, KY" ,description: "tasty" ,proof: 90,expert_score: 91, avg_rating: 3.80,bottle_image: "http://www.buffalotracedistillery.com/sites/default/files/Weller_CYPB_750ml_front_LoRes.png")
     @b2 = Bourbon.create(name: "Bourbon", distillery: "Buffalo Trace" ,location: "Dankfort, KY" ,description: "tasty" ,proof: 80,expert_score: 89, avg_rating: 3.84,bottle_image: "http://")
     @b3 = Bourbon.create(name: "Bourbon2", distillery: "Buffalo Trace" ,location: "Frankfort, CO" ,description: "tasty" ,proof: 95,expert_score: 87, avg_rating: 3.2,bottle_image: "http://")
+    @p1 = @b1.awards.create(name: "SF awards",award_type: "Gold", year: 2013,image: "http://")
   end
 
   describe "bourbon index" do
@@ -73,11 +74,79 @@ RSpec.describe "a user visiting the main page" do
       end
 
       it "shows the cards for bourbon" do
-        visit "/bourbons"
 
+        visit "/bourbons"
         expect(page).to have_css("div.bourbon-card", count:3)
       end
 
     end
+@b1 = Bourbon.create(name: "Bourbon", distillery: "Buffalo Trace" ,location: "Frankfort, KY" ,description: "tasty" ,proof: 90,expert_score: 91, avg_rating: 3.80,bottle_image: "http://")
+
+    context "in the bourbon cards" do
+      it "shows the name of the bourbon" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_content("Bourbon")
+        end
+      end
+
+      it "shows the bottle of bourbon" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_css("img.award-card")
+        end
+      end
+
+      it "shows the distillery" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_content("Buffalo Trace")
+        end
+      end
+
+      it "shows the proof" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_content("Proof: 90")
+        end
+      end
+
+      it "shows the expert score" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_content("Expert Score: 91")
+        end
+      end
+
+      it "shows the average rating" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_content("Average Rating: 3.8")
+        end
+      end
+
+      it "shows the description" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to  have_content("tasty")
+        end
+      end
+
+      it "shows the awards" do
+        visit "/bourbons"
+        within first(".bourbon-card") do
+          expect(page).to have_css("div.award-card", count:1)
+        end
+      end
+      #
+      # it "" do
+      #   visit "/bourbons"
+      #   within ".bourbon-card" do
+      #     expect(page).to
+      #   end
+      # end
+
+    end
   end
+
 end

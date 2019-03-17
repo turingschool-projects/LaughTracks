9478a8d72bc2bbb170908f9c7a8b31a03ad219f8
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-17 10:04:14 -0600

    adds comedians/new path to controller

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,8 +1,24 @@
 class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
+<<<<<<< Updated upstream
     @comedians = Comedian.all
     @specials = Special.all
     erb :comedians
   end
+=======
+    if params[:age]
+      @comedians = Comedian.where(age: params[:age])
+      @specials = Special.where(comedian_id: @comedians.ids)
+    else
+      @comedians = Comedian.all
+      @specials = Special.all
+    end
+    erb :comedians
+  end
+
+  get '/comedians/new' do
+    erb :new
+  end
+>>>>>>> Stashed changes
 end

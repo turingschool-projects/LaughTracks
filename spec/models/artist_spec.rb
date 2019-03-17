748b616de1f49d5600add4266d66521b2a5cc725
Jon Peterson <jon@pnwebs.com>
2019-03-16 20:07:32 -0600

    Add validation tests for Artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,12 +2,22 @@ RSpec.describe Artist do
   describe 'Validations' do
     describe 'Required Field(s)' do
       it 'should be invalid if missing a name' do
-        artist = Artist.create(formation_year: 1997)
+        artist = Artist.create(formation_year: 1997, origin: "Red Lion, PA", album_count: 4)
         expect(artist).to_not be_valid
       end
 
-      it 'should be invalid if missing an age' do
-        artist = Artist.create(name: 'Halestorm')
+      it 'should be invalid if missing a formation year' do
+        artist = Artist.create(name: "Halestorm", origin: "Red Lion, PA", album_count: 4)
+        expect(artist).to_not be_valid
+      end
+
+      it 'should be invalid if missing an origin city' do
+        artist = Artist.create(name: "Halestorm", album_count: 4)
+        expect(artist).to_not be_valid
+      end
+
+      it 'should be invalid if missing an album count' do
+        artist = Artist.create(name: "Halestorm", origin: "Red Lion, PA")
         expect(artist).to_not be_valid
       end
     end

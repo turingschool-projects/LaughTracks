9e8ea6e8d824757d44ca099a4d64b27dfa2ad800
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-03-17 09:15:03 -0600

    Added paths to create a new comedian and view each comedian on a page

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -6,7 +6,24 @@ class LaughTracksApp < Sinatra::Base
     @comedians = Comedian.all
     @specials = Special.all
     @age = params[:age]
+    @all_ages = Comedian.all_ages
     erb :comedians
+  end
+
+  get '/comedians/new' do
+    erb :new
+  end
+
+  post "/comedians" do
+    Comedian.create(name: params[:comedian][:name],
+                age: params[:comedian][:age],
+                city: params[:comedian][:city])
+    redirect '/comedians'
+  end
+
+  get '/comedians/:id' do
+    @comedian = Comedian.find(params["id"])
+    erb :show
   end
 
 end

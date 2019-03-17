42203fa034ad29307e44f576f7051cb4c8a6a0a5
Jon Peterson <jon@pnwebs.com>
2019-03-16 21:12:32 -0600

    Pass photo filename test

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,15 @@
 class Album < ActiveRecord::Base
   validates :name, :release_date, :track_count, :duration, presence: true
   belongs_to :artist
+
+  def photo
+    path = artist.name.downcase.gsub(" ", "_") + "/"
+    path += normalize_album_name(name)
+  end
+
+  def normalize_album_name(name)
+    name = name.downcase.gsub(" ", "_")
+    name = name.gsub("&", "and").gsub("+", "plus").gsub("#", "no")
+    name = name.gsub(/[:!?()',.-]/,"").gsub(/\_\_+/,"_")
+  end
 end

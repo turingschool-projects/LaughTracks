0851d4d9c3dff140b6d2fd092501db61b5928925
Jon Peterson <jon@pnwebs.com>
2019-03-16 20:10:47 -0600

    Add validation tests for Album

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,7 +2,30 @@ RSpec.describe Album do
   describe 'Validations' do
     describe 'Required Field(s)' do
       it 'should be invalid if missing a name' do
-        album = Album.create
+        album = Album.create(release_date: "2018-07-27",
+                             track_count: 12,
+                             duration: "43:26")
+        expect(album).to_not be_valid
+      end
+
+      it 'should be invalid if missing a release date' do
+        album = Album.create(name: "Vicious",
+                             track_count: 12,
+                             duration: "43:26")
+        expect(album).to_not be_valid
+      end
+
+      it 'should be invalid if missing a track count' do
+        album = Album.create(name: "Vicious",
+                             release_date: "2018-07-27",
+                             duration: "43:26")
+        expect(album).to_not be_valid
+      end
+
+      it 'should be invalid if missing a duration' do
+        album = Album.create(name: "Vicious",
+                             release_date: "2018-07-27",
+                             track_count: 12)
         expect(album).to_not be_valid
       end
     end

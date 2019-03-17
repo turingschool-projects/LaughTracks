951d4fea946b0e2813409074b21d092b37be90bb
Rob Stringer <stringer525@gmail.com>
2019-03-16 20:15:46 -0600

    Added calculation and grouping functionality and testing for story statistics.

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -4,4 +4,14 @@ class Comedian < ActiveRecord::Base
     validates :name, presence: true
     validates :age, presence: true
     
+
+    def self.average_age
+       Comedian.average(:age).round
+    end
+
+    def self.unique_hometowns
+        unique_comedians = Comedian.select(:birthplace).distinct
+        unique_hometowns = unique_comedians.map {|comedian| comedian.birthplace}
+        unique_hometowns.join(", ")
+    end
 end

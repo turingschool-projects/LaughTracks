02854dd9b0b39b8699f0e7217a6174ebe7454ddc
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-17 11:03:17 -0600

    completes new view and test

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe Special do
   describe 'New Comedian Form workflow' do
     it 'should add a new comedian to the database' do
-      within 'new-comedian' do
+      # within 'new-comedian' do
         visit '/comedians/new'
-        fill_in '#name', with: 'My New Comedian'
-        fill_in '#age', with: '35'
-        fill_in '#birthplace', with: 'Denver, CO'
+        fill_in 'name', with: 'My New Comedian'
+        fill_in 'age', with: '35'
+        fill_in 'birthplace', with: 'Denver, CO'
         click_on 'submit'
-      end
-      
+      # end
+
       expect(current_path).to be '/comedians'
       expect(page).to have_content('My New Comedian')
     end

a3b443f6616c71bba70e1fbbae4327c8f635dc07
Jon Peterson <jon@pnwebs.com>
2019-03-17 19:55:29 -0600

    Add route for sorting artists

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -4,6 +4,8 @@ class LaughTracksApp < Sinatra::Base
   get '/artists' do
     if params[:year]
       @artists = Artist.where(formation_year: params[:year])
+    elsif params[:sort]
+      @artists = Artist.order(params[:sort])
     else
       @artists = Artist.all
     end

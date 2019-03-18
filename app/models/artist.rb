bb28f1a08171286b2fb73598b1abb7abda3504ca
Jon Peterson <jon@pnwebs.com>
2019-03-17 19:48:19 -0600

    Add method to get average album length

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,4 +9,9 @@ class Artist < ActiveRecord::Base
   def self.average_age
     average("date_part('year',age(make_timestamp(formation_year,1,1,0,0,0.0)))").to_i
   end
+
+  def self.average_album_length
+    avg_length = joins(:albums).average("split_part(duration,':',1)::int + split_part(duration,':',2)::int / 60")
+    "#{avg_length.to_int}:#{((avg_length % 1) * 60).to_i.to_s.rjust(2, "0")}"
+  end
 end

de339ee99773285181b3418e2d84fac0dba5d49f
Jon Peterson <jon@pnwebs.com>
2019-03-17 19:54:19 -0600

    Add route for getting artists from a particular year

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,7 +2,11 @@ class LaughTracksApp < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + '../public'
 
   get '/artists' do
+    if params[:year]
+      @artists = Artist.where(formation_year: params[:year])
+    else
       @artists = Artist.all
+    end
     erb :index
   end
 end

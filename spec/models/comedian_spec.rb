4493dac1d1c0643cc3fd78c18e4ac3f8bd18108e
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-03-17 20:57:08 -0600

    Added tests to sort by name age and city

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Comedian do
     c1 = Comedian.create(name: "Patton Oswalt", age: 50, city: "Portsmouth")
     c2 = Comedian.create(name: "Whoopi Goldberg", age: 63, city: "New York City")
     c3 = Comedian.create(name: "Steve Martin", age: 73, city: "New York City")
+    c4 = Comedian.create(name: "Dane Cook", age: 51, city: "Brooklyn")
     c1.specials.create(comedian_id: 1, name: "Talking for Clapping", runtime: 65, thumb: "https://m.media-amazon.com/images/M/MV5BZmMyYzAzMzktOWM4MS00YmI2LThmM2MtNjBkODZkOWQxODNmXkEyXkFqcGdeQXVyNjYzMDA4MTI@._V1_UY268_CR3,0,182,268_AL_.jpg")
     c1.specials.create(comedian_id: 1, name: "Tragedy Plus Comedy Equals Time", runtime: 59, thumb: "https://m.media-amazon.com/images/M/MV5BODU2MDEyNGYtODE3ZC00YjUyLTk3ODctYTU2MzM5Y2M3NWM4XkEyXkFqcGdeQXVyMTk3NDAwMzI@._V1_UY268_CR0,0,182,268_AL_.jpg")
     c1.specials.create(comedian_id: 1, name: "No Reason to Complain", runtime: 42, thumb: "https://m.media-amazon.com/images/M/MV5BMTk3MjUxODI5NF5BMl5BanBnXkFtZTcwMjcxMTIzMQ@@._V1_UY268_CR4,0,182,268_AL_.jpg")
@@ -29,8 +30,8 @@ RSpec.describe Comedian do
     describe 'Calculations' do
 
       it 'can calculate average age' do
-        expected = 62
-        actual = Comedian.average_age
+        expected = 59.25
+        actual = Comedian.average_age.to_f.round(2)
         expect(actual).to eq(expected)
       end
     end
@@ -39,7 +40,7 @@ RSpec.describe Comedian do
     describe 'Collections' do
 
       it 'can list all comedians cities' do
-        expected = "Portsmouth, New York City"
+        expected = ['Brooklyn', 'New York City', 'Portsmouth']
         actual = Comedian.all_cities
         expect(actual).to eq(expected)
       end
@@ -53,6 +54,33 @@ RSpec.describe Comedian do
         expect(actual).to eq(expected)
       end
     end
-    
+
+    describe 'Sorting' do
+
+      it 'can sort page by comedian name' do
+        @comedians = Comedian.sort_by_name
+
+        expected = "Dane Cook"
+        actual = @comedians.first.name
+        expect(actual).to eq(expected)
+      end
+
+      it 'can sort page by comedian age' do
+        @comedians = Comedian.sort_by_age
+
+        expected = "Patton Oswalt"
+        actual = @comedians.first.name
+        expect(actual).to eq(expected)
+      end
+
+      it 'can sort page by comedian city' do
+        @comedians = Comedian.sort_by_city
+
+        expected = "Dane Cook"
+        actual = @comedians.first.name
+        expect(actual).to eq(expected)
+      end
+    end
+
   end
 end
